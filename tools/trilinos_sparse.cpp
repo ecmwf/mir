282d189fd57d972186761be7be70b6d6354b7f22
Tiago Quintino <tiago.quintino@ecmwf.int>
2013-10-10 13:59:38 +0100

    examples of Eigen and Trilinos

---
+++ tools/trilinos_sparse.cpp
@@ -0,0 +1,11 @@
+#include <vector>
+#include <algorithm> 
+#include <iostream>
+
+#include "utils.h"
+
+//-----------------------------------------------------------------------------------------
+
+int main()
+{
+}
