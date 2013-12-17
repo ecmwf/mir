396a3a0f190113bc5be0e432c2f51e8c9cbb3f75
Peter Bispham <peter.bispham@ecmwf.int>
2013-12-17 11:12:18 +0000

    added a data value test for identical input and output grids

--- tests/test_interpolate.cc
+++ tests/test_interpolate.cc
@@ -34,12 +34,14 @@ public:
     virtual void run();
 
     void test_constructor();
+    void test_values();
 };
 
 //-----------------------------------------------------------------------------
 
 void TestInterpolate::test_constructor()
 {
+    // test the integrity of the objects not the values
     using namespace eckit::grid;
 
     BoundBox2D earth ( Point2D(-90.,0.), Point2D(90.,360.) );
@@ -84,26 +86,27 @@ void TestInterpolate::test_constructor()
     eckit::grid::Field* f = new eckit::grid::Field(g, m, raw_data);
     fv.push_back(f);
 
+    // check it's the same object
+    ASSERT(&(f->metadata()) == m);
+
     // put it all into an INPUT field set
     eckit::grid::FieldSet input(fv);
 
     int count = 0;
     for (Field::Vector::iterator it = input.fields().begin(); it != input.fields().end(); ++it)
     {
-        std::cout << "Input field " << count++ << std::endl;
         // test the data that is in the grid
         const Field::Data& d = (*it)->data();
         for (unsigned int i = 0; i < raw_data->size(); i++)
         {
             ASSERT((*raw_data)[i] == d[i]);
-            std::cout << " input point " << i << " = " << d[i] << std::endl;
         }
     }
 
     // now assemble and output field set
     //
     Field::Vector fv1;
-    int n1 = 8;
+    int n1 = 4;
     Grid* g1 = new LatLon( n1, n1, earth );
 
     ASSERT( g1 );
@@ -128,19 +131,77 @@ void TestInterpolate::test_constructor()
     for (Field::Vector::iterator it = output.fields().begin(); it != output.fields().end(); ++it)
     {
 
-        std::cout << "Output Field " << count++ << std::endl;
-        // extract and test the data
-        if (!*it)
-            continue;
+        ASSERT(*it);
 
+        const Field::Data& d = (*it)->data();
+        // check the interpolator added data
+        ASSERT(d.size() > 0);
+    }
+
+}
+
+void TestInterpolate::test_values()
+{
+    using namespace eckit::grid;
+
+    BoundBox2D earth ( Point2D(-90.,0.), Point2D(90.,360.) );
+
+    // standard case
+    const size_t n = 4;
+    const size_t npts =  (n + 1) * (n + 1);
+
+    Grid* g = new LatLon( n, n, earth );
+
+    // make up some data for this field
+    eckit::grid::Field::Data* raw_data = new eckit::grid::Field::Data;
+    for (unsigned int i = 0; i <= 4; i++)
+    {
+        raw_data->push_back((double)i);
+        raw_data->push_back((double)i);
+        raw_data->push_back((double)i);
+        raw_data->push_back((double)i);
+        raw_data->push_back((double)i);
+    }
+
+    const std::vector<Point2D>& coords = g->coordinates();
+
+    eckit::grid::Field::MetaData* m = new eckit::grid::Field::MetaData();
+
+    Field::Vector fv;
+    eckit::grid::Field* f = new eckit::grid::Field(g, m, raw_data);
+    fv.push_back(f);
+
+    // put it all into an INPUT field set
+    eckit::grid::FieldSet input(fv);
+
+    // now assemble and output field set
+    //
+    Field::Vector fv1;
+    // test the same sized grid - should get the same answer as the input grid
+    Grid* g1 = new LatLon( n, n, earth );
+
+    eckit::grid::Field::MetaData* m1 = new eckit::grid::Field::MetaData();
+    
+    eckit::grid::Field* f1 = new eckit::grid::Field(g1, m1, new std::vector<double>);
+    fv1.push_back(f1);
+
+    // put it all into an OUTPUT field set
+    eckit::grid::FieldSet output(fv1);
+ 
+    // construct a bilinear interpolator
+    mir::Interpolator interp;
+
+    interp.interpolate(input, output);
+
+    for (Field::Vector::iterator it = output.fields().begin(); it != output.fields().end(); ++it)
+    {
         const Field::Data& d = (*it)->data();
         
         // check the interpolator added data
-        ASSERT(d.size() > 0);
         for (unsigned int i = 0; i < d.size(); i++)
         {
-            /// @todo we need to test the output data here
-           std::cout << "output point " << i << " = " << d[i] << std::endl;
+            /// @todo use standard epsilon value here
+            ASSERT(std::fabs(d[i] - (*raw_data)[i]) < 1.0e-10);
         }
     }
 
@@ -151,6 +212,7 @@ void TestInterpolate::test_constructor()
 void TestInterpolate::run()
 {
     test_constructor();
+    test_values();
 }
 
 //-----------------------------------------------------------------------------
