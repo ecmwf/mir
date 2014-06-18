816032340c8b582ea808427eea0232aa79bfe9f1
Tiago Quintino <tiago.quintino@ecmwf.int>
2014-06-18 17:54:34 +0100

    add  masks

---
+++ src/mir/Masks.cc
@@ -0,0 +1,60 @@
+/*
+ * (C) Copyright 1996-2014 ECMWF.
+ *
+ * This software is licensed under the terms of the Apache Licence Version 2.0
+ * which can be obtained at http://www.apache.org/licenses/LICENSE-2.0.
+ * In applying this licence, ECMWF does not waive the privileges and immunities
+ * granted to it by virtue of its status as an intergovernmental organisation nor
+ * does it submit to any jurisdiction.
+ */
+
+#include <string>
+
+#include "atlas/grid/Grid.h"
+#include "atlas/grid/Tesselation.h"
+
+#include "mir/Weights.h"
+#include "mir/WeightCache.h"
+
+//------------------------------------------------------------------------------------------------------
+
+using namespace atlas::grid;
+
+namespace mir {
+
+//------------------------------------------------------------------------------------------------------
+
+Weights::Weights()
+{
+}
+
+Weights::~Weights()
+{
+}
+
+void Weights::assemble(const Grid &in, const Grid &out, Eigen::SparseMatrix<double>& W ) const
+{
+    WeightCache cache;
+    const std::string whash = hash(in, out);
+    bool wcached = cache.get( whash, W );
+    if( ! wcached )
+    {
+        std::cout << ">>> computing weights ..." << std::endl;
+
+        compute( const_cast<Grid&>(in), const_cast<Grid&>(out), W );
+
+        cache.add( whash, W );
+    }
+    
+}
+
+std::string Weights::hash(const Grid &in, const Grid &out) const
+{
+    return classname() + std::string(".") +
+           in.hash() + std::string(".") +
+           out.hash();
+}
+
+//------------------------------------------------------------------------------------------------------
+
+} // namespace mir
---
+++ src/mir/Masks.h
@@ -0,0 +1,67 @@
+/*
+ * (C) Copyright 1996-2014 ECMWF.
+ *
+ * This software is licensed under the terms of the Apache Licence Version 2.0
+ * which can be obtained at http://www.apache.org/licenses/LICENSE-2.0.
+ * In applying this licence, ECMWF does not waive the privileges and immunities
+ * granted to it by virtue of its status as an intergovernmental organisation nor
+ * does it submit to any jurisdiction.
+ */
+
+/// @author Tiago Quintino
+/// @date June 2014
+
+#ifndef mir_Masks_H
+#define mir_Masks_H
+
+#include <string>
+#include <Eigen/Sparse>
+
+#include "eckit/memory/NonCopyable.h"
+
+#include "atlas/grid/Grid.h"
+#include "atlas/grid/FieldSet.h"
+
+//------------------------------------------------------------------------------------------------------
+
+namespace mir {
+
+//------------------------------------------------------------------------------------------------------
+
+class Masks : private eckit::NonCopyable {
+
+public: // types
+
+    typedef atlas::grid::Grid        Grid;
+    typedef atlas::grid::Grid::Point Point;
+    typedef atlas::grid::FieldHandle FieldHandle;
+    typedef atlas::grid::FieldSet    FieldSet;
+
+    typedef Eigen::SparseMatrix<double> WeightMatrix;
+
+public: // methods
+
+    Masks();
+
+    virtual ~Masks();
+
+    virtual std::string classname() const = 0;
+
+    WeightMatrix assemble( const FieldHandle& mask,
+                           const Grid& inp,
+                           const Grid& out,
+                           WeightMatrix& W ) const;
+
+protected: // methods
+
+    virtual void compute( const FieldHandle& mask,
+                          const Grid& inp,
+                          const Grid& out,
+                          WeightMatrix& W ) const = 0;
+};
+
+//------------------------------------------------------------------------------------------------------
+
+} // namespace mir
+
+#endif
