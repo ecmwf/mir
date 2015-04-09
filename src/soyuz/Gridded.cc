ea203b45ce1376c1b76eb4ffe1e975a286692e55
Pedro Maciel <Pedro.Maciel@ecmwf.int>
2015-04-09 11:13:59 +0100

    added intermediate class Gridded (Representation-Gridded-RegularLatLon)

---
+++ src/soyuz/Gridded.cc
@@ -0,0 +1,13 @@
+// File Gridded.cc
+// Baudouin Raoult - (c) ECMWF Apr 15
+
+#include "Gridded.h"
+
+
+Gridded::Gridded() {}
+
+
+Gridded::Gridded(const MIRParametrisation &parametrisation) {}
+
+
+Gridded::~Gridded() {}
