afc9e434963dc15275869cd450d15f5eadd7f8cd
Pedro Maciel <Pedro.Maciel@ecmwf.int>
2015-04-10 20:39:36 +0100

    Methods are named on creation (will be useful later)

--- src/soyuz/method/Method.h
+++ src/soyuz/method/Method.h
@@ -1,14 +1,27 @@
-// File Method.h
-// Baudouin Raoult - (c) ECMWF Apr 15
+/*
+ * (C) Copyright 1996-2015 ECMWF.
+ *
+ * This software is licensed under the terms of the Apache Licence Version 2.0
+ * which can be obtained at http://www.apache.org/licenses/LICENSE-2.0.
+ * In applying this licence, ECMWF does not waive the privileges and immunities
+ * granted to it by virtue of its status as an intergovernmental organisation nor
+ * does it submit to any jurisdiction.
+ */
 
-#ifndef Method_H
-#define Method_H
+/// @author Baudouin Raoult
+/// @author Pedro Maciel
+/// @date Apr 2015
+
+#ifndef soyuz_method_Method_H
+#define soyuz_method_Method_H
 
 #include <string>
 #include <iosfwd>
 
+
 class MIRParametrisation;
 class MIRField;
+
 
 class Method {
 public:
@@ -33,6 +46,8 @@ public:
 // -- Methods
 
     virtual void execute(MIRField&) const = 0;
+
+    const std::string& name() const { return name_; }
 
 // -- Overridden methods
 	// None
@@ -69,7 +84,11 @@ private:
 	Method& operator=(const Method&);
 
 // -- Members
-	// None
+
+    // name created methods, allowing builder to name them
+    std::string name_;
+    std::string& name() { return name_; }
+    template< class T > friend class MethodBuilder;
 
 // -- Methods
 	// None
@@ -85,14 +104,15 @@ private:
 
 // -- Friends
 
-	friend std::ostream& operator<<(std::ostream& s,const Method& p)
-		{ p.print(s); return s; }
+    friend std::ostream& operator<<(std::ostream& s,const Method& p)
+    { p.print(s); return s; }
 
 };
 
+
 class MethodFactory {
     std::string name_;
-    virtual Method* make(const MIRParametrisation&) = 0 ;
+    virtual Method* make(const std::string&, const MIRParametrisation&) = 0 ;
 
 protected:
 
@@ -104,9 +124,15 @@ public:
 
 };
 
+
 template<class T>
 class MethodBuilder : public MethodFactory {
-    virtual Method* make(const MIRParametrisation& param) { return new T(param); }
+    virtual Method* make(const std::string& name, const MIRParametrisation& param)
+    {
+        T* baby = new T(param);
+        baby->name() = name;
+        return baby;
+    }
 public:
     MethodBuilder(const std::string& name) : MethodFactory(name) {}
 };
