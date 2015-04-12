c67a8b0275f98b443a6bb1874cc363f2d8827ad6
Baudouin Raoult <baudouin.raoult@ecmwf.int>
2015-04-12 10:10:22 +0100

    Add code review for weight cache

--- src/soyuz/method/WeightCache.cc
+++ src/soyuz/method/WeightCache.cc
@@ -49,16 +49,22 @@ I was asked to review that code, so there it is:
 9 - There is no check done that the code used to read the file matched the code used to right the file.
     Old and new version of MIR (i.e. of MARS clients) will coexists. This should be refelected in the file
     name, e.g. adding a version number of the encoding code (no need for fancy SHA1, just 1, 2, 3...)
-10- Use size_t instead of unsigned int
-11- Use camelCase: filename() => fileName()
+10- SparseMatrixBase<T>::innerSize() return Index, not long. Code should reflect that (consider point 9).
+    This applies to many other variables.
+11- Variable ntrips should be size_t
+12- Use size_t instead of unsigned int
+13- Use camelCase: filename() => fileName()
+14- Log() message should be English sentences, and thus start with a capital letter
 
-The last two points could be addressed externally, i.e. having different cache directories
+The points 8 and 9 could be addressed externally, i.e. having different cache directories
 for different architecture, compilers, etc, but:
 
 a - Doing so will make it more difficult to share cache files between different applications (MARS, mir tool,
     PRODGEN, ...)
 b - Code should ASSERT() that what their are decoding looks correct. This can be done by sticking a header
     in front of the files, and cheching that the decoder can understand the header correctly.
+
+Point 12-13 are stylistic but important if you are serious about a common code base.
 
 TODO:
 1 - Create a generic file caching class in eckit that takes care of management of directories and files,
