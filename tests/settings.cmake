1e520eec0bd01785c655bc7503eb57fa4c2bbf8a
Pedro Maciel <Pedro.Maciel@ecmwf.int>
2016-02-16 16:06:58 +0000

    MIR-110: regression suite with several tests, using as reference the Emoslib results -- to be updated on the next commit

---
+++ tests/settings.cmake
@@ -0,0 +1,52 @@
+# Helper functions to abstract interpolation tools/environments
+
+
+# Easily add interpolation tests
+# (optional arg #5: test dependencies, default none)
+# (optional arg #6: test condition, default none)
+function( interpolation_add_test_interpol
+        _label
+        _file1
+        _file2
+        _options )
+    set( _depends "" )
+    if( ${ARGC} GREATER 4 )
+        set( _depends "${ARGV4}" )
+    endif()
+    set( _condition "" )
+    if( ${ARGC} GREATER 5 )
+        set( _condition "${ARGV5}" )
+    endif()
+    ecbuild_add_test(
+        TARGET       ${_label}_interpol
+        TEST_DEPENDS ${_depends}
+        CONDITION    ${_condition}
+        DEPENDS      mir_tool
+        COMMAND      ${CMAKE_BINARY_DIR}/bin/mir_tool
+        ARGS         ${_options} "${_file1}" "${_file2}" )
+endfunction()
+
+
+# Easily add comparison-to-reference results tests
+# (optional arg #4: test dependencies, default none)
+# (optional arg #5: strict comparison, default TRUE)
+function( interpolation_add_test_compare
+        _label
+        _file1
+        _file2 )
+    set( _depends "" )
+    if( ${ARGC} GREATER 3 )
+        set( _depends "${ARGV3}" )
+    endif()
+    set( _tolerate "" )
+    if( ${ARGC} GREATER 4 AND NOT ${ARGV4} )
+        set( _tolerate "--relative=1" )  #FIXME
+    endif()
+    ecbuild_add_test(
+        TARGET       ${_label}_compare
+        TEST_DEPENDS ${_label}_interpol ${_depends}
+        DEPENDS      mir_compare
+        COMMAND      ${CMAKE_BINARY_DIR}/bin/mir_compare
+        ARGS         ${_tolerate} "${_file1}" "${_file2}" )
+endfunction()
+
