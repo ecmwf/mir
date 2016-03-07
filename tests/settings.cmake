099e0c669bb5778707441adb7b754a337c81635e
Pedro Maciel <Pedro.Maciel@ecmwf.int>
2016-03-07 14:49:04 +0000

    MIR-110: added additional regression tests (incl. "single-point" areas) (sync with libemos)

--- tests/settings.cmake
+++ tests/settings.cmake
@@ -3,24 +3,23 @@
 
 # Easily add interpolation tests
 # (optional arg #5: test dependencies, default none)
-# (optional arg #6: test condition, default none)
 function( interpolation_add_test_interpol
         _label
         _file1
         _file2
         _options )
-    set( _depends "" )
+    set( _depends     "" )
+#   set( _condition   "" )
     if( ${ARGC} GREATER 4 )
         set( _depends "${ARGV4}" )
     endif()
-    set( _condition "" )
     if( ${ARGC} GREATER 5 )
-        set( _condition "${ARGV5}" )
+#       set( _condition "${ARGV5}" )
     endif()
     ecbuild_add_test(
         TARGET       ${_label}_interpol
         TEST_DEPENDS ${_depends}
-        CONDITION    ${_condition}
+#       CONDITION    ${_condition}
         DEPENDS      mir_tool
         COMMAND      ${CMAKE_BINARY_DIR}/bin/mir_tool
         ARGS         ${_options} "${_file1}" "${_file2}" )
@@ -29,7 +28,6 @@ endfunction()
 
 # Easily add comparison-to-reference results tests
 # (optional arg #4: test dependencies, default none)
-# (optional arg #5: strict comparison, default TRUE)
 function( interpolation_add_test_compare
         _label
         _file1
@@ -39,7 +37,7 @@ function( interpolation_add_test_compare
         set( _depends "${ARGV3}" )
     endif()
     set( _tolerate "" )
-    if( ${ARGC} GREATER 4 AND NOT ${ARGV4} )
+    if( FALSE )
         set( _tolerate "--relative=1" )  #FIXME
     endif()
     ecbuild_add_test(
