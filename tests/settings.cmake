158405b28bc2ff96ee633149fd5c31285a089014
Pedro Maciel <Pedro.Maciel@ecmwf.int>
2016-03-30 16:58:02 +0100

    MIR-116: added grib_api environment to testing tools

--- tests/settings.cmake
+++ tests/settings.cmake
@@ -1,28 +1,61 @@
 # Helper functions to abstract interpolation tools/environments
 
 
+# Set testing environment
+if( NOT grib_api_BASE_DIR )
+  unset( CMD_GRIB_COMPARE )
+
+  if( TARGET grib_compare )
+    get_target_property( CMD_GRIB_COMPARE grib_compare LOCATION )
+  endif()
+  if( NOT CMD_GRIB_COMPARE )
+    find_program( CMD_GRIB_COMPARE grib_compare NO_DEFAULT_PATH HINTS "${grib_api_BASE_DIR}" PATH_SUFFIXES "bin" )
+  endif()
+  if( NOT CMD_GRIB_COMPARE )
+    find_program( CMD_GRIB_COMPARE grib_compare )
+  endif()
+
+  if( CMD_GRIB_COMPARE )
+    get_filename_component( _grib_compare_dir "${CMD_GRIB_COMPARE}" PATH )
+    get_filename_component( grib_api_BASE_DIR "${_grib_compare_dir}" ABSOLUTE )
+    set( grib_api_BASE_DIR "${grib_api_BASE_DIR}/../" )
+  endif()
+
+  unset( CMD_GRIB_COMPARE )
+endif()
+
+unset( _grib_environment )
+if( grib_api_BASE_DIR )
+  set( _grib_environment
+    GRIB_DEFINITION_PATH=${grib_api_BASE_DIR}/share/grib_api/definitions/:${grib_api_BASE_DIR}/share/grib_api/definitions/grib1/
+    GRIB_SAMPLES_PATH=${grib_api_BASE_DIR}/share/grib_api/samples/ )
+endif()
+ecbuild_info( "Test _grib_environment: ${_grib_environment}" )
+
+
 # Easily add interpolation tests
 # (optional arg #5: test dependencies, default none)
+# (optional arg #6: environment variables, default none additional)
 function( interpolation_add_test_interpol
         _label
         _file1
         _file2
         _options )
     set( _depends     "" )
-#   set( _condition   "" )
+    set( _environment "" )
     if( ${ARGC} GREATER 4 )
         set( _depends "${ARGV4}" )
     endif()
     if( ${ARGC} GREATER 5 )
-#       set( _condition "${ARGV5}" )
+        set( _environment "${ARGV5}" )
     endif()
     ecbuild_add_test(
         TARGET       ${_label}_interpol
         TEST_DEPENDS ${_depends}
-#       CONDITION    ${_condition}
         DEPENDS      mir_tool
         COMMAND      ${CMAKE_BINARY_DIR}/bin/mir_tool
-        ARGS         ${_options} "${_file1}" "${_file2}" )
+        ARGS         ${_options} "${_file1}" "${_file2}"
+        ENVIRONMENT  ${_environment} ${_grib_environment} )
 endfunction()
 
 
@@ -45,6 +78,7 @@ function( interpolation_add_test_compare
         TEST_DEPENDS ${_label}_interpol ${_depends}
         DEPENDS      mir_compare
         COMMAND      ${CMAKE_BINARY_DIR}/bin/mir_compare
-        ARGS         ${_tolerate} "${_file1}" "${_file2}" )
+        ARGS         ${_tolerate} "${_file1}" "${_file2}"
+        ENVIRONMENT  ${_grib_environment} )
 endfunction()
 
