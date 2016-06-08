caf3d870c68196c950bb2fee951c6e43b4d242db
Baudouin Raoult <baudouin.raoult@ecmwf.int>
2016-06-08 08:25:14 +0100

    Empty

---
+++ src/mir/input/EmptyInput.cc
@@ -0,0 +1,118 @@
+/*
+ * (C) Copyright 1996-2015 ECMWF.
+ *
+ * This software is licensed under the terms of the Apache Licence Version 2.0
+ * which can be obtained at http://www.apache.org/licenses/LICENSE-2.0.
+ * In applying this licence, ECMWF does not waive the privileges and immunities
+ * granted to it by virtue of its status as an intergovernmental organisation nor
+ * does it submit to any jurisdiction.
+ */
+
+/// @author Baudouin Raoult
+/// @author Pedro Maciel
+/// @date Apr 2015
+
+
+#include <cmath>
+#include <iostream>
+
+#include "mir/data/MIRField.h"
+
+#include "mir/input/EmptyInput.h"
+
+
+namespace mir {
+namespace input {
+
+
+EmptyInput::EmptyInput(): calls_(0) {
+}
+
+
+EmptyInput::~EmptyInput() {}
+
+
+bool EmptyInput::sameAs(const MIRInput& other) const {
+    const EmptyInput* o = dynamic_cast<const EmptyInput*>(&other);
+    return o;
+}
+
+bool EmptyInput::next() {
+    return calls_++ == 0;
+}
+
+
+const param::MIRParametrisation &EmptyInput::parametrisation() const {
+    return *this;
+}
+
+
+data::MIRField *EmptyInput::field() const {
+    data::MIRField *field = new data::MIRField(*this, false, 999.);
+    return field;
+}
+
+
+void EmptyInput::print(std::ostream &out) const {
+    out << "EmptyInput[...]";
+}
+
+// bool EmptyInput::has(const std::string& name) const {
+//     if(name == "gridded") {
+//         return true;
+//     }
+//     if(name == "spectral") {
+//         return false;
+//     }
+//     return FieldParametrisation::has(name);
+// }
+
+// bool EmptyInput::get(const std::string &name, std::string &value) const {
+
+//     if (name == "gridType") {
+//         value = "regular_ll";
+//         return true;
+//     }
+//     return FieldParametrisation::get(name, value);
+// }
+
+// bool EmptyInput::get(const std::string &name, double &value) const {
+
+//     if (name == "north") {
+//         value = 90;
+//         return true;
+//     }
+
+//     if (name == "south") {
+//         value = -90;
+//         return true;
+//     }
+
+//     if (name == "west") {
+//         value = 0;
+//         return true;
+//     }
+
+//     if (name == "east") {
+//         value = 359;
+//         return true;
+//     }
+
+//     if (name == "west_east_increment") {
+//         value = 1;
+//         return true;
+//     }
+
+//     if (name == "south_north_increment") {
+//         value = 1;
+//         return true;
+//     }
+
+//     return FieldParametrisation::get(name, value);
+// }
+
+
+
+}  // namespace input
+}  // namespace mir
+
---
+++ src/mir/output/EmptyOutput.h
@@ -0,0 +1,119 @@
+/*
+ * (C) Copyright 1996-2015 ECMWF.
+ *
+ * This software is licensed under the terms of the Apache Licence Version 2.0
+ * which can be obtained at http://www.apache.org/licenses/LICENSE-2.0.
+ * In applying this licence, ECMWF does not waive the privileges and immunities
+ * granted to it by virtue of its status as an intergovernmental organisation nor
+ * does it submit to any jurisdiction.
+ */
+
+/// @author Baudouin Raoult
+/// @author Pedro Maciel
+/// @date Apr 2015
+
+
+#ifndef DummyOutput_H
+#define DummyOutput_H
+
+#include "mir/output/MIROutput.h"
+
+
+namespace eckit {
+class DataHandle;
+}
+
+
+namespace mir {
+namespace output {
+
+
+class EmptyOutput : public MIROutput {
+  public:
+
+// -- Exceptions
+    // None
+
+// -- Contructors
+
+    EmptyOutput();
+
+// -- Destructor
+
+    ~EmptyOutput(); // Change to virtual if base class
+
+// -- Convertors
+    // None
+
+// -- Operators
+    // None
+
+// -- Methods
+    // None
+
+// -- Overridden methods
+    // None
+
+// -- Class members
+    // None
+
+// -- Class methods
+    // None
+
+  protected:
+
+// -- Members
+    // None
+
+// -- Methods
+
+
+// -- Overridden methods
+    // None
+
+// -- Class members
+    // None
+
+// -- Class methods
+    // None
+
+  private:
+
+// No copy allowed
+
+    EmptyOutput(const EmptyOutput&);
+    EmptyOutput& operator=(const EmptyOutput&);
+
+// -- Members
+
+// -- Methods
+    // None
+
+
+// -- Overridden methods
+    // From MIROutput
+    virtual void print(std::ostream&) const; // Change to virtual if base class
+    virtual bool sameAs(const MIROutput& other) const;
+
+    virtual size_t copy(const param::MIRParametrisation&, input::MIRInput&); // Not iterpolation performed
+    virtual size_t save(const param::MIRParametrisation&, input::MIRInput&, data::MIRField&);
+
+
+// -- Class members
+    // None
+
+// -- Class methods
+    // None
+
+// -- Friends
+
+    //friend ostream& operator<<(ostream& s,const EmptyOutput& p)
+    // { p.print(s); return s; }
+
+};
+
+
+}  // namespace output
+}  // namespace mir
+#endif
+
