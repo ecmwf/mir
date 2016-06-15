6103f05ceb4d190e1a43fe6a486c567e7f36f69c
Baudouin Raoult <baudouin.raoult@ecmwf.int>
2016-06-15 07:47:26 +0100

    Refactoring

--- src/mir/api/MIRComplexJob.cc
+++ src/mir/api/MIRComplexJob.cc
@@ -76,14 +76,13 @@ void MIRComplexJob::execute(util::MIRStatistics& statistics) const {
     }
 
     eckit::ScopedPtr< data::MIRField > field(input_->field());
-    eckit::Log::trace<MIR>() << "Field is " << *field << std::endl;
 
-    std::cout << ">>>>>>>>>>>> ====== ";
+    std::cout << ">>>>>>>>>>>> ====== " << std::endl;
 
     std::cout << *input_ << std::endl;
-    std::cout << *field << std::endl;
+    // std::cout << *field << std::endl;
 
-    graph.dump(std::cout, 0);
+    graph.dump(std::cout, 1);
     graph.execute(*field, statistics);
     std::cout << "<<<<<<<<<<< ======" << std::endl;
 
