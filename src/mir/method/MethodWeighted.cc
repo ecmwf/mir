5556fdbe5ed6d35ce5a97b6231ab43a941a44d5d
Baudouin Raoult <baudouin.raoult@ecmwf.int>
2016-07-17 18:30:02 +0100

    Keep 64 matrices in cache

--- src/mir/method/MethodWeighted.cc
+++ src/mir/method/MethodWeighted.cc
@@ -56,7 +56,7 @@ namespace method {
 
 namespace {
 static eckit::Mutex local_mutex;
-static InMemoryCache<WeightMatrix> matrix_cache("mirMatrices", 32);
+static InMemoryCache<WeightMatrix> matrix_cache("mirMatrices", 64);
 static InMemoryCache<atlas::mesh::Mesh> mesh_cache("mirMeshes", 2);
 }
 
