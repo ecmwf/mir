f6d3b98127254ef877f6558b43d8cb982221dcb8
Florian Rathgeber <florian.rathgeber@ecmwf.int>
2016-09-19 15:41:53 +0100

    bamboo: use grib_api on opensuse131

--- bamboo/opensuse131-flags.cmake
+++ bamboo/opensuse131-flags.cmake
@@ -1,2 +1,3 @@
 #TODO re-enable when ready
 set( ENABLE_MIR_TESTS_REGRESS OFF         CACHE STRING "additional tests: regression" )
+set( ENABLE_ECCODES           OFF         CACHE STRING "Use grib_api on bamboo" )
