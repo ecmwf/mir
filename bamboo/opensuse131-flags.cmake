6bc1ea013817d701e93ded8ddf7286b392613e68
Pedro Maciel <Pedro.Maciel@ecmwf.int>
2016-09-28 14:50:55 +0100

    Remove regression tests

--- bamboo/opensuse131-flags.cmake
+++ bamboo/opensuse131-flags.cmake
@@ -1,3 +1,2 @@
 #TODO re-enable when ready
-set( ENABLE_MIR_TESTS_REGRESS OFF         CACHE STRING "additional tests: regression" )
 set( ENABLE_ECCODES           OFF         CACHE STRING "Use grib_api on bamboo" )
