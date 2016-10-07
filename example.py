b1574accd0e52ab1e26e5824e50a2f55b06c41a8
Florian Rathgeber <florian.rathgeber@ecmwf.int>
2016-10-07 12:28:23 +0100

    Add example

---
+++ example.py
@@ -0,0 +1,12 @@
+import mir
+
+# Retrieve some data as GRIB, e.g.
+#
+#   echo retrieve,param=z,target=z.grib | mars
+
+# Create a MIR Job
+job = mir.MIRJob().set('grid', '1.0/1.0')
+
+# Run the job on the retrieved GRIB file
+print 'Running', job
+job.execute(mir.GribFileInput('z.grib'), mir.GribFileOutput('z_ll.grib'))
