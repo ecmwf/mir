aba8486813b016cdf82e5c9d33d1a6ea55d3cb33
Florian Rathgeber <florian.rathgeber@ecmwf.int>
2016-10-11 10:34:13 +0100

    Disable assertion tests on bamboo

--- bamboo/flags.cmake
+++ bamboo/flags.cmake
@@ -1,2 +1,3 @@
 #TODO re-enable when ready
-set( ENABLE_ECCODES OFF         CACHE STRING "Use grib_api on bamboo" )
+set( ENABLE_ECCODES         OFF CACHE STRING "Use grib_api on bamboo" )
+set( ENABLE_TEST_ASSERTIONS OFF CACHE STRING "Disable assertion tests" )
