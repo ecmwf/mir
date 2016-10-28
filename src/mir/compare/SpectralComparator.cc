93e85539776de607ba6a184618bec4bd8edc500d
Pedro Maciel <Pedro.Maciel@ecmwf.int>
2016-10-28 18:36:26 +0100

    SpectralComparator

---
+++ src/mir/compare/SpectralComparator.cc
@@ -0,0 +1,111 @@
+/*
+ * (C) Copyright 1996-2016 ECMWF.
+ *
+ * This software is licensed under the terms of the Apache Licence Version 2.0
+ * which can be obtained at http://www.apache.org/licenses/LICENSE-2.0.
+ * In applying this licence, ECMWF does not waive the privileges and immunities
+ * granted to it by virtue of its status as an intergovernmental organisation nor
+ * does it submit to any jurisdiction.
+ */
+
+/// @date Oct 2016
+
+#include <cmath>
+
+#include "eckit/exception/Exceptions.h"
+#include "mir/compare/SpectralComparator.h"
+#include "mir/config/LibMir.h"
+#include "mir/data/MIRField.h"
+#include "mir/param/MIRParametrisation.h"
+#include "mir/util/Compare.h"
+#include "eckit/exception/Exceptions.h"
+
+
+namespace mir {
+namespace compare {
+
+
+SpectralComparator::SpectralComparator(const param::MIRParametrisation& param1, const param::MIRParametrisation& param2) {
+#if 0
+    double absoluteError1;
+    double absoluteError2;
+    ASSERT(param1.get("absolute-error", absoluteError1));
+    ASSERT(param2.get("absolute-error", absoluteError2));
+    ASSERT(absoluteError1 == absoluteError2);
+
+    absoluteError_ = absoluteError1;
+    ASSERT(absoluteError_ > 0);
+#endif
+}
+
+
+SpectralComparator::~SpectralComparator() {
+}
+
+void SpectralComparator::execute(const data::MIRField& field1, const data::MIRField& field2) const {
+    ASSERT(field1.dimensions() == field2.dimensions());
+
+    for (size_t w = 0; w < field1.dimensions(); ++w) {
+
+        const std::vector<double>& values1 = field1.values(w);
+        const std::vector<double>& values2 = field2.values(w);
+#if 0
+        ASSERT(values1.size() == values2.size());
+
+        if (field1.hasMissing() || field2.hasMissing()) {
+            NOTIMP;
+
+            util::compare::IsMissingFn isMissing1(field1.hasMissing()? field1.missingValue() : std::numeric_limits<double>::quiet_NaN());
+            util::compare::IsMissingFn isMissing2(field2.hasMissing()? field2.missingValue() : std::numeric_limits<double>::quiet_NaN());
+
+            for (size_t i = 0; i < values1.size(); ++i) {
+                if (isMissing1(values1[i] != isMissing1(values2[i]))) {
+
+                    std::ostringstream oss;
+                    oss << "missing value mismatch at point " << (i+1);
+                    throw eckit::UserError(oss.str());
+
+                } else if (!isMissing1(values1[i] && !isMissing1(values2[i]))) {
+
+
+                }
+            }
+
+        } else {
+
+            for (size_t i = 0; i < values1.size(); ++i) {
+                double diff = std::abs(values1[i] - values2[i]);
+                if (diff > absoluteError_) {
+
+                    std::ostringstream oss;
+                    oss << "value mismatch at point " << (i+1) << ": V1=" << values1[i] << ", V2=" << values2[i] << ", |V2-V1|=" << diff;
+                    throw eckit::UserError(oss.str());
+
+                }
+            }
+
+        }
+#endif
+
+    }
+}
+
+
+void SpectralComparator::print(std::ostream& out) const {
+    out << "SpectralComparator["
+#if 0
+        << "absoluteError=" << absoluteError_
+#endif
+        << "]";
+}
+
+
+namespace {
+ComparatorBuilder<SpectralComparator> __spectralComparator("spectral");
+}
+
+
+}  // namespace compare
+}  // namespace mir
+
+
