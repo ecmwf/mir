69f59803f1cab3f1dddd9b699bb0681e7f92e33c
Pedro Maciel <Pedro.Maciel@ecmwf.int>
2017-01-19 18:46:07 +0000

    MIR-166: nwq Trans-based vo/d-to-U/V transformation (IFS-like)

--- src/mir/action/transform/Vod2uv.cc
+++ src/mir/action/transform/Vod2uv.cc
@@ -20,11 +20,25 @@
 #include "eckit/exception/Exceptions.h"
 #include "atlas/util/Constants.h"
 #include "mir/action/context/Context.h"
+#include "mir/action/transform/TransInitor.h"
 #include "mir/config/LibMir.h"
 #include "mir/data/MIRField.h"
 #include "mir/param/MIRParametrisation.h"
 #include "mir/repres/sh/SphericalHarmonics.h"
 #include "mir/util/MIRStatistics.h"
+
+#ifdef ATLAS_HAVE_TRANS
+#include "transi/trans.h"
+
+#define TRANS_CHECK( CALL ) do {\
+  int errcode = CALL;\
+  if( errcode != TRANS_SUCCESS) {\
+    printf("ERROR: %s failed @%s:%d:\n%s\n",#CALL,__FILE__,__LINE__,trans_error_msg(errcode));\
+    abort();\
+  }\
+} while(0)
+
+#endif
 
 
 namespace mir {
@@ -53,17 +67,8 @@ void Vod2uv::print(std::ostream &out) const {
 }
 
 
-inline double dd(double pm, double pn) {
-    return -sqrt((pn * pn - pm * pm) / (4.*pn * pn - 1)) / pn;
-}
-
-
-inline double ss(double pm, double pn) {
-    return -pm / (pn * (pn + 1));
-}
-
-
 void Vod2uv::execute(context::Context & ctx) const {
+#ifdef ATLAS_HAVE_TRANS
     data::MIRField& field = ctx.field();
 
     ASSERT(field.dimensions() == 2);
@@ -87,95 +92,28 @@ void Vod2uv::execute(context::Context & ctx) const {
     ASSERT(field_vo.size() == size);
     ASSERT(field_d.size() == size);
 
-    std::vector<double> result_u(size, 0);
-    std::vector<double> result_v(size, 0);
+    std::vector<double> result_U(size, 0);
+    std::vector<double> result_V(size, 0);
 
-    std::vector<double> temp_vo;
-    std::vector<double> temp_d;
-
-
-    repres::sh::SphericalHarmonics::truncate(truncation, truncation - 1, field_vo, temp_vo);
-    repres::sh::SphericalHarmonics::truncate(truncation, truncation - 1, field_d, temp_d);
-
-
-    typedef std::vector<std::complex<double> > veccomp;
-    const veccomp &vorticity = reinterpret_cast<const veccomp &>(temp_vo);
-    const veccomp &divergence = reinterpret_cast<const veccomp &>(temp_d);
-
-    veccomp &u_component = reinterpret_cast<veccomp &>(result_u);
-    veccomp &v_component = reinterpret_cast<veccomp &>(result_v);
+    struct VorDivToUV_t vod_to_UV = new_vordiv_to_UV();
+    vod_to_UV.nfld   = 1;
+    vod_to_UV.ncoeff = static_cast<int>(size);        // number of coefficients
+    vod_to_UV.nsmax  = static_cast<int>(truncation);  // spectral resolution (T)
+    vod_to_UV.rspvor = field_vo.data();
+    vod_to_UV.rspdiv = field_d.data();
+    vod_to_UV.rspu   = result_U.data();
+    vod_to_UV.rspv   = result_V.data();
 
 
-    // ref. libemos/gribex/vod2uv.F
-
-    std::complex<double> zi(0.0, 1.0);
-    const double kRadiusOfTheEarth = atlas::util::Earth::radiusInMeters();
-    size_t k = 0;
-    size_t imn = 0;
-
-    size_t count = truncation;
+    TransInitor::instance(); // Will init trans if needed
+    TRANS_CHECK( trans_vordiv_to_UV(&vod_to_UV) );
 
 
-    for (size_t j = 0 ; j < count ;  j++) {
-        double zm = j;
-        double zn = zm;
-
-        double ddmn1 = dd(zm, zn + 1.);
-        double ssmn = ss(zm, zn);
-        if (j) {
-            u_component[k] = (-ddmn1 * vorticity[imn + 1] + zi * ssmn * divergence[imn]) * kRadiusOfTheEarth;
-            v_component[k] = ( ddmn1 * divergence[imn + 1] + zi * ssmn * vorticity[imn]) * kRadiusOfTheEarth;
-
-        } else {
-            u_component[k] = (-ddmn1 * vorticity[imn + 1]) * kRadiusOfTheEarth ;
-            v_component[k] = ( ddmn1 * divergence[imn + 1]) * kRadiusOfTheEarth;
-        }
-
-        imn++;
-        k++;
-        size_t  jmp = j + 1;
-
-        if (jmp < count - 1) {
-            for (size_t i = jmp; i < count - 1;  i++) {
-                zn = i;
-
-                double ddzmn = dd(zm, zn);
-                double ddmn1 = dd(zm, zn + 1.);
-                double ssmn = ss(zm, zn);
-                u_component[k] =  ( ddzmn * vorticity[imn - 1] - ddmn1 * vorticity[imn + 1] + zi * ssmn * divergence[imn]) * kRadiusOfTheEarth;
-                v_component[k] =  (-ddzmn * divergence[imn - 1] + ddmn1 * divergence[imn + 1] + zi * ssmn * vorticity[imn]) * kRadiusOfTheEarth;
-                k++;
-                imn++;
-            }
-
-            zn = count - 1;
-            double ddzmn = dd(zm, zn);
-            double ssmn = ss(zm, zn);
-            u_component[k] =  ( ddzmn * vorticity[imn - 1] + zi * ssmn * divergence[imn]) * kRadiusOfTheEarth;
-            v_component[k] =  (-ddzmn * divergence[imn - 1] + zi * ssmn * vorticity[imn]) * kRadiusOfTheEarth;
-            k++;
-            imn++;
-        }
-
-        zn = count;
-        double ddzmn = dd(zm, zn);
-        u_component[k] =  ddzmn * vorticity[imn - 1] * kRadiusOfTheEarth;
-        v_component[k] =  -ddzmn * divergence[imn - 1] * kRadiusOfTheEarth;
-        k++;
-
-    }
-
-    while (2 * k < size) {
-        u_component[k] = 0;
-        v_component[k] = 0;
-        k++;
-    }
-
-    field.update(result_u, 0);
-    field.update(result_v, 1);
+    field.update(result_U, 0);
+    field.update(result_V, 1);
 
 
-    // configure paramIds for u/v
+    // configure paramIds for U/V
     long id_u = 131;
     long id_v = 132;
     parametrisation_.get("transform.vod2uv.u", id_u);
@@ -183,6 +121,10 @@ void Vod2uv::execute(context::Context & ctx) const {
 
     field.metadata(0, "paramId", id_u);
     field.metadata(1, "paramId", id_v);
+#else
+    throw eckit::SeriousBug("Spherical harmonics transforms are not supported. "
+                            "Please recompile ATLAS with TRANS support enabled.");
+#endif
 }
 
 
