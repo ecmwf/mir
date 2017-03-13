e855c9e934ffe741ae2cce7e94b2eaf797bff89b
Baudouin Raoult <baudouin.raoult@ecmwf.int>
2017-03-13 23:02:49 +0000

    Missing truncation

--- src/mir/style/DisseminationStyle.cc
+++ src/mir/style/DisseminationStyle.cc
@@ -61,7 +61,9 @@ void DisseminationStyle::sh2grid(action::ActionPlan& plan) const {
 
 
 void DisseminationStyle::shTruncate(action::ActionPlan& plan) const {
-    // do nothing
+    if (parametrisation_.has("user.truncation")) {
+        plan.add("transform.sh-truncate");
+    }
 }
 
 
