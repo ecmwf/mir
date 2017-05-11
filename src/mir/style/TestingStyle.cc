261e57a22474c7df568253e3be1849e5d529e86b
Pedro Maciel <Pedro.Maciel@ecmwf.int>
2017-05-11 16:37:09 +0100

    MIR-147: remove table(s) and that old stuff

--- src/mir/style/TestingStyle.cc
+++ src/mir/style/TestingStyle.cc
@@ -38,7 +38,7 @@ TestingStyle::~TestingStyle() {
 void TestingStyle::prepare(action::ActionPlan &) const {
     using namespace action::transform::mapping;
 
-    for (auto m : {"table", "linear", "quadratic", "cubic"}) {
+    for (auto m : {"linear", "quadratic", "cubic"}) {
         eckit::ScopedPtr<Mapping> map(action::transform::mapping::MappingFactory::build(m, parametrisation_));
         ASSERT(map.get());
 
