089f9f7c8179923358f7dad43fc12da7b28fd4d6
Pedro Maciel <Pedro.Maciel@ecmwf.int>
2017-07-20 19:28:39 +0100

    Attempt a more compatible implementation of NearestLSM

--- src/mir/method/NearestLSMSingleKDTree.cc
+++ src/mir/method/NearestLSMSingleKDTree.cc
@@ -16,14 +16,19 @@
 
 #include "mir/method/NearestLSMSingleKDTree.h"
 
+//#include <forward_list>
+//#include <utility>
 #include "eckit/log/Log.h"
+#include "eckit/log/BigNum.h"
+#include "eckit/log/ETA.h"
+#include "eckit/log/Plural.h"
 #include "eckit/log/Timer.h"
 #include "mir/config/LibMir.h"
 #include "mir/lsm/LandSeaMasks.h"
 #include "mir/param/RuntimeParametrisation.h"
 #include "mir/repres/Iterator.h"
 #include "mir/repres/Representation.h"
-#include "mir/util/Compare.h"
+//#include "mir/util/Compare.h"
 #include "mir/util/PointSearch.h"
 
 
@@ -64,6 +69,18 @@ void NearestLSMSingleKDTree::assemble(util::MIRStatistics&,
     eckit::Log::debug<LibMir>() << "NearestLSMSingleKDTree compute LandSeaMasks " << timer.elapsed() - here << std::endl;
 
 
+    // TODO take from parametrisation, plenty of examples out there
+    const size_t nclosest = 4;
+    std::vector<util::PointSearch::PointValueType> closest;
+
+
+//    typedef std::pair< size_t, repres::Iterator::point_ll_t > different_type_t;
+//    std::forward_list<different_type_t> different_types;
+    size_t nbDifferentTypes = 0;
+
+    const size_t out_npts = out.numberOfPoints();
+
+
     // compute masked/not-masked search trees
     here = timer.elapsed();
 
@@ -72,10 +89,9 @@ void NearestLSMSingleKDTree::assemble(util::MIRStatistics&,
     ASSERT(imask.size() == W.cols());
     ASSERT(omask.size() == W.rows());
 
-    util::PointSearch sptree_masked(parametrisation_, in, util::compare::IsMaskedFn   (imask));
-    util::PointSearch sptree_notmasked(parametrisation_, in, util::compare::IsNotMaskedFn(imask));
+    util::PointSearch sptree(parametrisation_, in);
 
-    eckit::Log::debug<LibMir>() << "NearestLSMSingleKDTree compute masked/not-masked search trees " << timer.elapsed() - here << std::endl;
+    eckit::Log::debug<LibMir>() << "NearestLSMSingleKDTree compute search tree " << timer.elapsed() - here << std::endl;
 
 
     // search nearest neighbours matching in/output masks
@@ -86,30 +102,76 @@ void NearestLSMSingleKDTree::assemble(util::MIRStatistics&,
     std::vector<WeightMatrix::Triplet> mat;
     mat.reserve(W.rows());
 
-    const eckit::ScopedPtr<repres::Iterator> it(out.iterator());
-    size_t ip = 0;
-    while (it->next()) {
-        ASSERT(ip < W.rows());
+    {
+        eckit::Log::debug<LibMir>() << "Locating same-type neighbours (up to " << nclosest << ") for " << eckit::Plural(out_npts, "output point") << " from input " << in << std::endl;
+        eckit::TraceTimer<LibMir> timerLocating("Locating");
 
-        // pick the (input) search tree matching the output mask
-        util::PointSearch& sptree(
-            omask[ip] ? sptree_masked
-            : sptree_notmasked );
+        // output points
+        const eckit::ScopedPtr<repres::Iterator> it(out.iterator());
+        size_t ip = 0;
+        while (it->next()) {
+            ASSERT(ip < W.rows());
 
-        // perform nearest neighbour search
-        // - p: output grid node to look neighbours for
-        // - q: input grid node closest to p (accessed as qmeta.point())
-        // - j: index of q in input grid (or input field)
-        const util::PointSearch::PointType      p(it->point3D());
-        const util::PointSearch::PointValueType qmeta = sptree.closestPoint(p);
-        const size_t jp = qmeta.payload();
+            if (ip && (ip % 10000 == 0)) {
+                double rate = ip / timerLocating.elapsed();
+                eckit::Log::debug<LibMir>()
+                        << eckit::BigNum(ip) << " ..."  << eckit::Seconds(timerLocating.elapsed())
+                        << ", rate: " << rate << " points/s, ETA: "
+                        << eckit::ETA( (out_npts - ip) / rate )
+                        << std::endl;
+            }
 
-        // insert entry into uncompressed matrix structure
-        mat.push_back(WeightMatrix::Triplet(ip, jp, 1.));
+            // perform nearest neighbour search
+            // - p: output grid node to look neighbours for
+            // - ip: matrix row, or output point index
+            // - jp: matrix column, or input point index
+            const util::PointSearch::PointType p(it->point3D());
+            sptree.closestNPoints(p, nclosest, closest);
+            ASSERT(!closest.empty());
 
-        ++ip;
+            bool foundSameType = false;
+            size_t jp = 0;
+
+            for (auto p : closest) {
+                jp = p.payload();
+                ASSERT(jp < imask.size());
+
+                if (omask[ip] == imask[jp]) {
+                    foundSameType = true;
+                    break;
+                }
+            }
+
+            if (!foundSameType) {
+                jp = closest.front().payload();
+//                different_types.push_front(different_type_t(ip, it->pointUnrotated()));
+                ++nbDifferentTypes;
+            }
+
+            // insert entry into uncompressed matrix structure
+            mat.push_back(WeightMatrix::Triplet(ip, jp, 1.));
+
+            ++ip;
+        }
     }
-    eckit::Log::debug<LibMir>() << "NearestLSMSingleKDTree search nearest neighbours matching in/output masks " << timer.elapsed() - here << std::endl;
+    eckit::Log::debug<LibMir>() << "NearestLSMSingleKDTree search nearest neighbours matching type " << timer.elapsed() - here << std::endl;
+
+
+    eckit::Log::debug<LibMir>() << "Assigned weights to " << eckit::Plural(nbDifferentTypes, "point") << " of different type, out of " << eckit::BigNum(out_npts) << std::endl;
+//    if (nbDifferentTypes) {
+//        std::stringstream msg;
+//        msg << "Assigned weights to " << eckit::Plural(nbDifferentTypes, "point") << " of different type";
+//        eckit::Log::debug<LibMir>() << msg.str() << ":";
+//        size_t count = 0;
+//        for (const different_type_t& f : different_types) {
+//            eckit::Log::debug<LibMir>() << "\n\tpoint " << f.first << " (lon, lat) = (" << f.second.lon.value() << ", " << f.second.lat.value() << ")";
+//            if (++count > 10) {
+//                eckit::Log::debug<LibMir>() << "\n\t...";
+//                break;
+//            }
+//        }
+//        eckit::Log::debug<LibMir>() << std::endl;
+//    }
 
 
     // fill-in sparse matrix
