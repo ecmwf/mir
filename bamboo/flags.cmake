37e0e81c625dba175af73b33022c09ee0e3ed355
Pedro Maciel <Pedro.Maciel@ecmwf.int>
2017-10-30 14:24:32 +0000

    Continuous Integration: enable assertion tests

--- bamboo/flags.cmake
+++ bamboo/flags.cmake
@@ -1,1 +1,2 @@
-set( ENABLE_TEST_ASSERTIONS OFF CACHE STRING "Disable assertion tests" )
+#set( ENABLE_TEST_ASSERTIONS OFF CACHE STRING "Disable assertion tests" )
+set( ENABLE_TEST_ASSERTIONS ON CACHE STRING "Enable assertion tests" )
