575cffedd17cfd55d83f1a47ea43e3baef0b59d8
Pedro Maciel <Pedro.Maciel@ecmwf.int>
2017-11-30 14:31:26 +0000

    Fix unit tests

--- src/mir/action/interpolate/Gridded2GridDef.h
+++ src/mir/action/interpolate/Gridded2GridDef.h
@@ -89,6 +89,7 @@ class Gridded2GridDef : public Gridded2GriddedInterpolation {
 // -- Overridden methods
 
     virtual bool sameAs(const Action& other) const;
+    virtual void custom(std::ostream &) const;
 
     // From Gridded2GriddedInterpolation
     virtual const repres::Representation* outputRepresentation() const;
