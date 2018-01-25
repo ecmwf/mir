cd167df30c2e153c3fc2e63ee093ce73bf46f1eb
Tiago Quintino <tiago.quintino@ecmwf.int>
2018-01-25 13:21:51 +0000

    Fix bamboo leap42

--- bamboo/CLANG-env.sh
+++ bamboo/CLANG-env.sh
@@ -7,5 +7,11 @@
 if [[ ! $(command -v module > /dev/null 2>&1) ]]; then
   . /usr/local/apps/module/init/bash
 fi
+
 module unload grib_api
+module unload eccodes
+module unload emos
+module unload fftw
+module unload libemos
+
 module switch gnu clang/3.6.2
