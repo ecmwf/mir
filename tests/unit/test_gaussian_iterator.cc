cec27ac46f8e4509a1ee50a2b83fca75a3688d63
Pedro Maciel <Pedro.Maciel@ecmwf.int>
2018-06-26 08:58:33 +0100

    test_gaussian_iterator

--- tests/unit/test_gaussian_iterator.cc
+++ tests/unit/test_gaussian_iterator.cc
@@ -9,6 +9,7 @@
  */
 
 
+#include <vector>
 #include "eckit/log/Log.h"
 #include "eckit/memory/ScopedPtr.h"
 #include "eckit/testing/Test.h"
@@ -28,10 +29,17 @@ using repres::RepresentationHandle;
 using util::BoundingBox;
 
 
-enum counting_mode_t { iterator, GRIB1, GRIB2 };
+static BoundingBox dummy;
 
 
-size_t numberOfPoints(const repres::Representation& repres, counting_mode_t counting, size_t gaussianNumber, size_t numberOfValues) {
+void numberOfPoints(
+        const repres::Representation& repres,
+        size_t gaussianNumber,
+        size_t numberOfValues,
+        size_t& numberOfValues1,
+        size_t& numberOfValues2,
+        size_t& numberOfValuesIterator,
+        BoundingBox& encodedBoundingBox = dummy) {
 
     // Make sure handles are deleted even in case of exception
     class HandleFree {
@@ -47,14 +55,6 @@ size_t numberOfPoints(const repres::Representation& repres, counting_mode_t coun
         }
     };
 
-    ASSERT(gaussianNumber > 0);
-    std::string sample("reduced_gg_pl_" + std::to_string(gaussianNumber) + std::string(counting == GRIB1 ? "_grib1" : "_grib2"));
-
-    grib_handle* handle = grib_handle_new_from_samples(nullptr, sample.c_str());
-    HandleFree handle_detroy(handle);
-    ASSERT(handle);
-
-
     // Fill info to pass to grib_util_set_spec
     // (with dummy paramId "Indicates a missing value" and values)
     int err = 0;
@@ -68,36 +68,120 @@ size_t numberOfPoints(const repres::Representation& repres, counting_mode_t coun
     info.packing.extra_settings[j].type = GRIB_TYPE_LONG;
     info.packing.extra_settings[j].long_value = 129255;
 
-    info.packing.editionNumber = long(counting == GRIB1 ? 1 : 2);
-
     repres.fill(info);
 
 
     // Encode
     int flags = 0;
-    grib_handle* result = grib_util_set_spec(handle, &info.grid, &info.packing, flags, &values[0], values.size(), &err);
-    HandleFree result_destroy(result);
-    GRIB_CALL(err);
 
 
-    // Get number of values
-    long n = 0;
-    if (counting == iterator) {
+    // Get encoded numberOfValues in GRIB1
+    {
+        ASSERT(gaussianNumber > 0);
+        std::string sample("reduced_gg_pl_" + std::to_string(gaussianNumber) + "_grib1");
 
-        grib_iterator* iter = grib_iterator_new(result, 0, &err);
+        grib_handle* handle = grib_handle_new_from_samples(nullptr, sample.c_str());
+        HandleFree handle_detroy(handle);
+        ASSERT(handle);
+
+        info.packing.editionNumber = 1L;
+
+        grib_handle* result1 = grib_util_set_spec(handle, &info.grid, &info.packing, flags, &values[0], values.size(), &err);
+        HandleFree result1_destroy(result1);
+        GRIB_CALL(err);
+
+        long n = 0;
+        grib_get_long(result1, "numberOfValues", &n);
+        ASSERT(n > 0);
+        numberOfValues1 = size_t(n);
+    }
+
+
+    // Get encoded numberOfValues in GRIB2, iterator, and bounding box
+    {
+        ASSERT(gaussianNumber > 0);
+        std::string sample("reduced_gg_pl_" + std::to_string(gaussianNumber) + "_grib2");
+
+        grib_handle* handle = grib_handle_new_from_samples(nullptr, sample.c_str());
+        HandleFree handle_detroy(handle);
+        ASSERT(handle);
+
+        info.packing.editionNumber = 2L;
+
+        grib_handle* result2 = grib_util_set_spec(handle, &info.grid, &info.packing, flags, &values[0], values.size(), &err);
+        HandleFree result2_destroy(result2);
+        GRIB_CALL(err);
+
+        long n = 0;
+        grib_get_long(result2, "numberOfValues", &n);
+        ASSERT(n > 0);
+        numberOfValues2 = size_t(n);
+
+        grib_iterator* iter = grib_iterator_new(result2, 0, &err);
         if (err != GRIB_SUCCESS) {
             GRIB_CHECK(err, nullptr);
         }
+
+        n = 0;
         for (double lat, lon, value; grib_iterator_next(iter, &lat, &lon, &value); ++n) {
         }
+        ASSERT(n > 0);
+
         grib_iterator_delete(iter);
+        numberOfValuesIterator = size_t(n);
 
-    } else {
-        grib_get_long(result, "numberOfValues", &n);
+        // Get encoded bounding box
+        double box[4];
+        grib_get_double(result2, "latitudeOfFirstGridPointInDegrees",  &box[0]);
+        grib_get_double(result2, "longitudeOfFirstGridPointInDegrees", &box[1]);
+        grib_get_double(result2, "latitudeOfLastGridPointInDegrees",   &box[2]);
+        grib_get_double(result2, "longitudeOfLastGridPointInDegrees",  &box[3]);
+
+        encodedBoundingBox = BoundingBox(box[0], box[1], box[2], box[3]);
     }
-    ASSERT(n > 0);
+}
 
-    return size_t(n);
+
+size_t areaCropper(const repres::Representation& repres, BoundingBox& bbox) {
+
+    Latitude n = 0;
+    Latitude s = 0;
+    Longitude e = 0;
+    Longitude w = 0;
+
+    size_t count = 0;
+    bool first = true;
+
+    // Iterator is "unrotated", because the cropping area
+    // is expressed in before the rotation is applied
+    eckit::ScopedPtr<repres::Iterator> iter(repres.iterator());
+    while (iter->next()) {
+        const repres::Iterator::point_ll_t& point = iter->pointUnrotated();
+        if (bbox.contains(point.lat, point.lon)) {
+
+            const Latitude& lat = point.lat;
+            const Longitude lon = point.lon.normalise(bbox.west());
+
+            if (first) {
+                n = s = lat;
+                e = w = lon;
+                first = false;
+            } else {
+                if (n < lat) { n = lat; }
+                if (s > lat) { s = lat; }
+                if (e < lon) { e = lon; }
+                if (w > lon) { w = lon; }
+            }
+
+            count++;
+        }
+    }
+
+    // Don't support empty results
+    ASSERT(count > 0);
+
+    bbox = BoundingBox(n, w, s, e);
+    return count;
 }
 
 
@@ -140,24 +224,32 @@ CASE("test area point count (O640)") {
         }) {
         log << "Test " << *grid << " with " << bbox << "..." << std::endl;
 
-        RepresentationHandle cropped(grid->croppedRepresentation(bbox));
+        // crop "manually", to get the smallest possible bounding box
+        BoundingBox small = bbox;
+        size_t n = areaCropper(*grid, small);
+        ASSERT(0 < n);
+        EXPECT(n < nGlobal);
+        log << "\tnumberOfPoints =\t" << n << std::endl;
 
-        size_t n = cropped->numberOfPoints();
+        RepresentationHandle cropped(grid->croppedRepresentation(bbox));
+        EXPECT(n == cropped->numberOfPoints());  // (heavy!)
+
+        // check point counts
         ASSERT(n > 0);
         EXPECT(n < nGlobal);
         log << "\tnumberOfPoints =\t" << n << std::endl;
 
-        size_t numberOfPointsIterator = numberOfPoints(*cropped, counting_mode_t::iterator, gaussianNumber, n);
-        EXPECT(numberOfPointsIterator == n);
-        log << "\tecCodes iterator =\t" << numberOfPointsIterator << std::endl;
-
-        size_t numberOfPoints1 = numberOfPoints(*cropped, counting_mode_t::GRIB1, gaussianNumber, n);
-        EXPECT(numberOfPoints1 == n);
-        log << "\tGRIB1 numberOfValues =\t" << n << std::endl;
-
-        size_t numberOfPoints2 = numberOfPoints(*cropped, counting_mode_t::GRIB2, gaussianNumber, n);
-        EXPECT(numberOfPoints2 == n);
-        log << "\tGRIB2 numberOfValues =\t" << n << std::endl;
+        size_t numberOfValues1 = 0;
+        size_t numberOfValues2 = 0;
+        size_t numberOfValuesIterator = 0;
+        numberOfPoints(*cropped, gaussianNumber, n, numberOfValues1, numberOfValues2, numberOfValuesIterator);
+        log <<   "\tGRIB1 numberOfValues =\t" << numberOfValues1
+            << "\n\tGRIB2 numberOfValues =\t" << numberOfValues2
+            << "\n\tecCodes iterator =\t" << numberOfValuesIterator
+            << std::endl;
+        EXPECT(numberOfValues1 == n);
+        EXPECT(numberOfValues2 == n);
+        EXPECT(numberOfValuesIterator == n);
     }
 }
 
@@ -178,24 +270,95 @@ CASE("test area point count (O1280)") {
         }) {
         log << "Test " << *grid << " with " << bbox << "..." << std::endl;
 
-        RepresentationHandle cropped(grid->croppedRepresentation(bbox));
-
-        size_t n = cropped->numberOfPoints();
+        // crop "manually", to get the smallest possible bounding box
+        BoundingBox small = bbox;
+        size_t n = areaCropper(*grid, small);
         ASSERT(0 < n);
         EXPECT(n < nGlobal);
         log << "\tnumberOfPoints =\t" << n << std::endl;
 
-        size_t numberOfPointsIterator = numberOfPoints(*cropped, counting_mode_t::iterator, gaussianNumber, n);
-        EXPECT(numberOfPointsIterator == n);
-        log << "\tecCodes iterator =\t" << numberOfPointsIterator << std::endl;
+        RepresentationHandle cropped(grid->croppedRepresentation(bbox));
+        EXPECT(n == cropped->numberOfPoints());  // (heavy!)
 
-        size_t numberOfPoints1 = numberOfPoints(*cropped, counting_mode_t::GRIB1, gaussianNumber, n);
-        EXPECT(numberOfPoints1 == n);
-        log << "\tGRIB1 numberOfValues =\t" << n << std::endl;
+        ASSERT(0 < n);
+        EXPECT(n < nGlobal);
+        log << "\tnumberOfPoints =\t" << n << std::endl;
 
-        size_t numberOfPoints2 = numberOfPoints(*cropped, counting_mode_t::GRIB2, gaussianNumber, n);
-        EXPECT(numberOfPoints2 == n);
-        log << "\tGRIB2 numberOfValues =\t" << n << std::endl;
+        size_t numberOfValues1 = 0;
+        size_t numberOfValues2 = 0;
+        size_t numberOfValuesIterator = 0;
+        numberOfPoints(*cropped, gaussianNumber, n, numberOfValues1, numberOfValues2, numberOfValuesIterator);
+        log <<   "\tGRIB1 numberOfValues =\t" << numberOfValues1
+            << "\n\tGRIB2 numberOfValues =\t" << numberOfValues2
+            << "\n\tecCodes iterator =\t" << numberOfValuesIterator
+            << std::endl;
+        EXPECT(numberOfValues1 == n);
+        EXPECT(numberOfValues2 == n);
+        EXPECT(numberOfValuesIterator == n);
+    }
+}
+
+
+CASE("ECC-445 (O1280)") {
+    auto& log = eckit::Log::info();
+
+    const size_t gaussianNumber(1280);
+    RepresentationHandle grid(new repres::gauss::reduced::ReducedOctahedral(gaussianNumber));
+
+    log << "Test " << *grid << std::endl;
+    size_t nGlobal = grid->numberOfPoints();
+    ASSERT(nGlobal > 0);
+    log << "\tnumberOfPoints =\t" << nGlobal << std::endl;
+
+    struct test_t {
+        size_t numberOfPoints;
+        BoundingBox bbox;
+    };
+
+    for (auto& test : {
+         test_t{  12369, {  37.6025, -114.891,  27.7626, -105.188 }},
+         test_t{     19, {  27.9,     253,      27.8,     254 }},
+         test_t{ 124577, { -10.017,   -85,     -38.981,   -56 }},
+         test_t{ 124209, { -10.0176,  275,     -38.9807,  304 }},
+         test_t{  12274, {  37.5747,  245.109,  27.8032,  254.812 }},
+         test_t{  12373, {  37.575,  -114.892,  27.803,  -105.187 }},
+         test_t{  12373, {  37.6025, -114.8915, 27.7626, -105.1875 }},
+         test_t{ 124143, { -10, -85,  -39,     -56.1 }},
+        }) {
+        log << "Test " << *grid << " with " << test.bbox << "..." << std::endl;
+
+        // crop "manually", to get the smallest possible bounding box
+        BoundingBox small = test.bbox;
+        size_t n = areaCropper(*grid, small);
+        ASSERT(0 < n);
+        EXPECT(n < nGlobal);
+        log << "\tnumberOfPoints =\t" << n << std::endl;
+
+        RepresentationHandle cropped(grid->croppedRepresentation(test.bbox));
+        EXPECT(n == cropped->numberOfPoints());  // (heavy!)
+
+        size_t numberOfValues1 = 0;
+        size_t numberOfValues2 = 0;
+        size_t numberOfValuesIterator = 0;
+        BoundingBox encoded;
+        numberOfPoints(*cropped, gaussianNumber, n, numberOfValues1, numberOfValues2, numberOfValuesIterator, encoded);
+
+        log <<   "\tGRIB1 numberOfValues =\t" << numberOfValues1
+            << "\n\tGRIB2 numberOfValues =\t" << numberOfValues2
+            << "\n\tecCodes iterator =\t" << numberOfValuesIterator
+            << std::endl;
+
+        EXPECT(numberOfValues1 == n);
+        EXPECT(numberOfValues2 == n);
+        EXPECT(numberOfValuesIterator == n);
+
+        log <<   "\tcrop =\t" << test.bbox
+            << "\n\tbbox =\t" << small << " (contained by crop)"
+            << "\n\tencoded = " << encoded << " (expanded bbox)"
+            << std::endl;
+        EXPECT(test.bbox.contains(small));
+        EXPECT(encoded.contains(small));
+
     }
 }
 
