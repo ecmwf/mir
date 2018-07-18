8332a81dbcb0520b6b57aa550a37383567590975
Pedro Maciel <Pedro.Maciel@ecmwf.int>
2018-07-18 12:33:31 +0100

    MIR-270: include bounding box edges mid-points to calculate rotated (min, max)

--- src/mir/action/interpolate/Gridded2RotatedGrid.cc
+++ src/mir/action/interpolate/Gridded2RotatedGrid.cc
@@ -12,6 +12,7 @@
 #include "mir/action/interpolate/Gridded2RotatedGrid.h"
 
 #include <vector>
+#include "eckit/exception/Exceptions.h"
 #include "eckit/types/FloatCompare.h"
 #include "mir/method/Method.h"
 #include "mir/param/MIRParametrisation.h"
@@ -50,31 +51,36 @@ const util::BoundingBox& Gridded2RotatedGrid::croppingBoundingBox() const {
     auto& bbox(method().hasCropping() ? method().getCropping()
                                       : out->boundingBox());
 
-    Latitude n = bbox.north();
-    Latitude s = bbox.south();
-    Longitude w = bbox.west();
-    Longitude e = bbox.east();
-
 
     // rotate bounding box corners and find (min, max)
+    // include edges mid-points as they curve in the rotated frame
     const atlas::PointLonLat southPole(
                 rotation_.south_pole_longitude().normalise(Longitude::GREENWICH).value(),
                 rotation_.south_pole_latitude().value() );
+    const atlas::util::Rotation R(southPole);
 
-    const atlas::util::Rotation r(southPole);
-    const atlas::PointLonLat p[] {
-        r.rotate({w.value(), n.value()}),
-        r.rotate({e.value(), n.value()}),
-        r.rotate({e.value(), s.value()}),
-        r.rotate({w.value(), s.value()})
-    };
+    Point2 min, max;
+    {
+        const double n = bbox.north().value();
+        const double s = bbox.south().value();
+        const double w = bbox.west().value();
+        const double e = bbox.east().value();
+        const atlas::PointLonLat p[] {
+            R.rotate({w, n}), R.rotate({(w + e) / 2., n}),
+            R.rotate({e, n}), R.rotate({e, (s + n) / 2.}),
+            R.rotate({e, s}), R.rotate({(w + e) / 2., s}),
+            R.rotate({w, s}), R.rotate({w, (s + n) / 2.}),
+        };
 
-    Point2 min(p[0]);
-    Point2 max(p[0]);
-    for (size_t i = 1; i < 4; ++i) {
-        min = Point2::componentsMin(min, p[i]);
-        max = Point2::componentsMax(max, p[i]);
+        min = max = p[0];
+        for (size_t i = 1; i < 8; ++i) {
+            min = Point2::componentsMin(min, p[i]);
+            max = Point2::componentsMax(max, p[i]);
+        }
     }
+    ASSERT(min[0] < max[0]);
+    ASSERT(min[1] < max[1]);
+
 
     //// extend by 'angle' latitude- and longitude-wise
     //constexpr double angle = 0.; //0.001 ??
@@ -83,8 +89,8 @@ const util::BoundingBox& Gridded2RotatedGrid::croppingBoundingBox() const {
 
 
     // check bbox including poles (in the unrotated frame)
-    atlas::PointLonLat NP{ r.unrotate({0., Latitude::NORTH_POLE.value()}) };
-    atlas::PointLonLat SP{ r.unrotate({0., Latitude::SOUTH_POLE.value()}) };
+    atlas::PointLonLat NP{ R.unrotate({0., Latitude::NORTH_POLE.value()}) };
+    atlas::PointLonLat SP{ R.unrotate({0., Latitude::SOUTH_POLE.value()}) };
 
     bool includesNorthPole = bbox.contains(NP.lat(), NP.lon())
             || eckit::types::is_approximately_lesser_or_equal(Latitude::NORTH_POLE.value(), max[1]);
@@ -95,10 +101,10 @@ const util::BoundingBox& Gridded2RotatedGrid::croppingBoundingBox() const {
     bool isPeriodicWestEast = includesNorthPole || includesSouthPole
             || eckit::types::is_approximately_lesser_or_equal(Longitude::GLOBE.value(), max[0] - min[0]);
 
-    n = includesNorthPole ? Latitude::NORTH_POLE : max[1];
-    s = includesSouthPole ? Latitude::SOUTH_POLE : min[1];
-    w = isPeriodicWestEast ? 0 : min[0];
-    e = isPeriodicWestEast ? Longitude::GLOBE : max[0];
+    Latitude n = includesNorthPole ? Latitude::NORTH_POLE : max[1];
+    Latitude s = includesSouthPole ? Latitude::SOUTH_POLE : min[1];
+    Longitude w = isPeriodicWestEast ? 0 : min[0];
+    Longitude e = isPeriodicWestEast ? Longitude::GLOBE : max[0];
 
     bbox_ = util::BoundingBox(n, w, s, e);
     return bbox_;
