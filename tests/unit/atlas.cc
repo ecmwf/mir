c78c0f6aa751ccf7b2299b55bd38dffa5517d5a2
Pedro Maciel <Pedro.Maciel@ecmwf.int>
2018-07-19 15:39:32 +0100

    More unit tests

--- tests/unit/atlas.cc
+++ tests/unit/atlas.cc
@@ -16,6 +16,7 @@
 #include "mir/api/Atlas.h"
 #include "mir/repres/latlon/RegularLL.h"
 #include "mir/namedgrids/NamedGrid.h"
+#include "mir/util/Domain.h"
 
 
 namespace mir {
@@ -44,6 +45,47 @@ CASE("Test number of points representation <=> grid") {
         auto grid = repres->atlasGrid();
         size_t n2 = grid.size();
         log << "#=" << n2 << "\tfrom " << grid.spec() << std::endl;
+
+        EXPECT(n1 == n2);
+    }
+}
+
+
+CASE("Test number of points representation <=> cropped grid") {
+    auto& log = eckit::Log::info();
+
+    using repres::latlon::RegularLL;
+
+    const util::Domain domains[] = {
+        {  90,    0,  90,  360 },  // North pole
+        {  90,    0,  89,  360 },
+        {   0,    0,   0,  360 },
+        { -89,    0, -90,  360 },
+        { -90,    0, -90,  360 },  // South pole
+        {  90,    0, -90,    0 },  // Greenwhich
+        {  90,    0, -90,    1 },
+        {  90,  180, -90,  180 },  // date line
+        {  90, -180, -90, -180 },  // date line
+        {  90,   -1, -90,    0 },
+        {  90,   -1, -90,   -1 },
+    };
+
+    for (const auto& dom : domains) {
+        ASSERT(!dom.isGlobal());
+
+        repres::RepresentationHandle repres(new RegularLL(util::Increments(1., 1.)));
+        repres::RepresentationHandle represCropped(repres->croppedRepresentation(dom));
+        size_t n1 = represCropped->numberOfPoints();
+        log << "#=" << n1 << "\tfrom " << *represCropped << std::endl;
+
+        auto grid = repres->atlasGrid();
+        ASSERT(grid.domain().global());
+
+        auto gridCropped = atlas::Grid(grid, dom);
+        ASSERT(!gridCropped.domain().global());
+
+        size_t n2 = gridCropped.size();
+        log << "#=" << n2 << "\tfrom " << gridCropped.spec() << std::endl;
 
         EXPECT(n1 == n2);
     }
