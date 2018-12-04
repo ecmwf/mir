c96c35f0617c31693af3426134291efb36f1fcc4
Pedro Maciel <Pedro.Maciel@ecmwf.int>
2018-12-04 17:18:53 +0000

    Gaussian grids area point counts

--- tests/unit/gaussian_grid.cc
+++ tests/unit/gaussian_grid.cc
@@ -34,34 +34,38 @@ CASE("NamedGrid") {
 
     struct test_t {
         const std::string gridname;
-        size_t numberOfPOints;
+        size_t numberOfGlobalPoints;
+        size_t numberOfLocalPoints;
         BoundingBox bbox;
     };
 
     std::vector<test_t> tests {
-        {"N128",     88838, BoundingBox( 90,         0,      -90,       359.297)},
-        {"F320",    819200, BoundingBox( 56,       -16.5,     50.5,      -3)},
-        {"N320",    542080, BoundingBox(-60,        50,      -90,       180)},
-        {"O320",    421120, BoundingBox( 58,      -130,       25,       -60)},
-        {"O320",    421120, BoundingBox(-25,       135,      -47,       179)},
-        {"O320",    421120, BoundingBox( 12,       -81,      -50,       -35)},
-        {"O320",    421120, BoundingBox( 44,        91,       21,       143)},
-        {"O320",    421120, BoundingBox( 60,        23,       35,        80)},
-        {"O640",   1661440, BoundingBox( 52,         7,       43,        27.7)},
-        {"N640",   2140702, BoundingBox(-60,        50,      -90,       180)},
-        {"O640",   1661440, BoundingBox( 58,      -130,       25,       -60)},
-        {"O640",   1661440, BoundingBox(-25,       135,      -47,       179)},
-        {"F1280", 13107200, BoundingBox( 75,       -30,       25,        60)},
-        {"O1280",  6599680, BoundingBox( 12,       -81,      -50,       -35)},
-        {"O1280",  6599680, BoundingBox( 37.6025, -114.8907,  27.7626, -105.1875)},
-        {"O1280",  6599680, BoundingBox( 90,         0,      -90,       359.929)},
-        {"O1280",  6599680, BoundingBox(-25,       135,      -47,       179)},
+        {"N128",     88838,   88838, BoundingBox( 90,         0,      -90,       359.297)},
+        {"F320",    819200,     912, BoundingBox( 56,       -16.5,     50.5,      -3)},
+        {"N320",    542080,   13437, BoundingBox(-60,        50,      -90,       180)},
+        {"O320",    421120,   16133, BoundingBox( 58,      -130,       25,       -60)},
+        {"O320",    421120,    7511, BoundingBox(-25,       135,      -47,       179)},
+        {"O320",    421120,   28101, BoundingBox( 12,       -81,      -50,       -35)},
+        {"O320",    421120,    9877, BoundingBox( 44,        91,       21,       143)},
+        {"O320",    421120,    8743, BoundingBox( 60,        23,       35,        80)},
+        {"O640",   1661440,    4512, BoundingBox( 52,         7,       43,        27.7)},
+        {"N640",   2140702,   52654, BoundingBox(-60,        50,      -90,       180)},
+        {"O640",   1661440,   63842, BoundingBox( 58,      -130,       25,       -60)},
+        {"O640",   1661440,   29674, BoundingBox(-25,       135,      -47,       179)},
+        {"F1280", 13107200,  910080, BoundingBox( 75,       -30,       25,        60)},
+        {"O1280",  6599680,  442460, BoundingBox( 12,       -81,      -50,       -35)},
+        {"O1280",  6599680,   12370, BoundingBox( 37.6025, -114.8907,  27.7626, -105.1875)},
+        {"O1280",  6599680, 6599646, BoundingBox( 90,         0,      -90,       359.929)},
+        {"O1280",  6599680,  118210, BoundingBox(-25,       135,      -47,       179)},
     };
 
     SECTION("Area of Gaussian grids numberOfPoints") {
         for (const auto& t : tests) {
-            repres::RepresentationHandle r(namedgrids::NamedGrid::lookup(t.gridname).representation());
-            EXPECT(r->numberOfPoints() == t.numberOfPOints);
+            repres::RepresentationHandle global(namedgrids::NamedGrid::lookup(t.gridname).representation());
+            EXPECT(global->numberOfPoints() == t.numberOfGlobalPoints);
+
+            repres::RepresentationHandle local(global->croppedRepresentation(t.bbox));
+            EXPECT(local->numberOfPoints() == t.numberOfLocalPoints);
         }
     }
 
