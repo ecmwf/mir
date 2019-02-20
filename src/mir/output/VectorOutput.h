95e63309e78bba710034a0ddba6c500f494d4cdc
Pedro Maciel <Pedro.Maciel@ecmwf.int>
2019-02-20 10:53:26 +0000

    MIR-346: correctly pass MIROutput when using VectorOutput/MultiScalarOuput (pe. winds)

--- src/mir/output/VectorOutput.h
+++ src/mir/output/VectorOutput.h
@@ -91,7 +91,7 @@ private:
     virtual bool sameAs(const MIROutput&) const;
     virtual bool sameParametrisation(const param::MIRParametrisation&, const param::MIRParametrisation&) const;
     virtual bool printParametrisation(std::ostream&, const param::MIRParametrisation&) const;
-    virtual void prepare(const param::MIRParametrisation&, action::ActionPlan&, input::MIRInput&);
+    virtual void prepare(const param::MIRParametrisation&, action::ActionPlan&, input::MIRInput&, output::MIROutput&);
     virtual void print(std::ostream&) const;
 
     // -- Class members
