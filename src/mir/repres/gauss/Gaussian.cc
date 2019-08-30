1ef48ad3de5463996c2b29cccc69d30a28f4a152
Pedro Maciel <Pedro.Maciel@ecmwf.int>
2019-08-30 17:14:11 +0100

    Gaussian::sameAs: use domain instead of bbox (it represents a crop of a global grid, so comparing corners is not relevant)

--- src/mir/repres/gauss/Gaussian.cc
+++ src/mir/repres/gauss/Gaussian.cc
@@ -26,11 +26,12 @@
 #include "eckit/types/FloatCompare.h"
 
 #include "mir/api/Atlas.h"
+#include "mir/api/MIREstimation.h"
 #include "mir/config/LibMir.h"
 #include "mir/param/MIRParametrisation.h"
+#include "mir/util/Domain.h"
 #include "mir/util/Grib.h"
 #include "mir/util/MeshGeneratorParameters.h"
-#include "mir/api/MIREstimation.h"
 
 
 namespace mir {
@@ -75,7 +76,7 @@ Gaussian::~Gaussian() = default;
 
 bool Gaussian::sameAs(const Representation& other) const {
     auto o = dynamic_cast<const Gaussian*>(&other);
-    return o && (N_ == o->N_) && (bbox_ == o->bbox_);
+    return o && (N_ == o->N_) && (domain() == o->domain());
 }
 
 
