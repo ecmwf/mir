b782f9a46129163d84377ffcfeeb49f544831abe
Pedro Maciel <Pedro.Maciel@ecmwf.int>
2019-09-27 10:23:16 +0100

    Cleanup

--- src/tools/mir-inspect.cc
+++ src/tools/mir-inspect.cc
@@ -1,20 +1,20 @@
 /*
-* (C) Copyright 1996- ECMWF.
-*
-* This software is licensed under the terms of the Apache Licence Version 2.0
-* which can be obtained at http://www.apache.org/licenses/LICENSE-2.0.
-* In applying this licence, ECMWF does not waive the privileges and immunities
-* granted to it by virtue of its status as an intergovernmental organisation nor
-* does it submit to any jurisdiction.
-*/
+ * (C) Copyright 1996- ECMWF.
+ *
+ * This software is licensed under the terms of the Apache Licence Version 2.0
+ * which can be obtained at http://www.apache.org/licenses/LICENSE-2.0.
+ * In applying this licence, ECMWF does not waive the privileges and immunities
+ * granted to it by virtue of its status as an intergovernmental organisation nor
+ * does it submit to any jurisdiction.
+ */
 
 /// @author Baudouin Raoult
 /// @author Pedro Maciel
 /// @date Apr 2015
 
 
-#include "eckit/utils/Tokenizer.h"
 #include "eckit/log/Log.h"
+#include "eckit/utils/Tokenizer.h"
 
 #include "mir/input/GribFileInput.h"
 #include "mir/tools/MIRTool.h"
@@ -26,49 +26,46 @@ class MIRInspect : public mir::tools::MIRTool {
 
     void execute(const eckit::option::CmdArgs&);
 
-    void usage(const std::string &tool) const;
+    void usage(const std::string& tool) const;
 
-    int minimumPositionalArguments() const {
-        return 1;
-    }
+    int minimumPositionalArguments() const { return 1; }
 
 public:
-
     // -- Contructors
 
-    MIRInspect(int argc, char **argv) : mir::tools::MIRTool(argc, argv) {}
-
+    MIRInspect(int argc, char** argv) : mir::tools::MIRTool(argc, argv) {}
 };
 
 
-void MIRInspect::usage(const std::string &tool) const {
+void MIRInspect::usage(const std::string& tool) const {
     eckit::Log::info()
-            << "\n" "Usage: " << tool << " [key1 key2 ...] file.grib"
-               "\n" "Examples:"
-               "\n" "% " << tool << " grid area file.grib"
-               "\n" "% " << tool << " reduced file.grib"
-               "\n" "% " << tool << " regular file.grib"
-               "\n" "% " << tool << " truncation file.grib"
-               "\n" "% " << tool << " octahedral file.grib"
-            << std::endl;
+        << "\n" "Usage: " << tool << " [key1 key2 ...] file.grib"
+           "\n" "Examples:"
+           "\n" "% " << tool << " grid area file.grib"
+           "\n" "% " << tool << " reduced file.grib"
+           "\n" "% " << tool << " regular file.grib"
+           "\n" "% " << tool << " truncation file.grib"
+           "\n" "% " << tool << " octahedral file.grib"
+        << std::endl;
 }
 
 void MIRInspect::execute(const eckit::option::CmdArgs&) {
 
     mir::input::GribFileInput file(argv(argc() - 1));
     while (file.next()) {
-        mir::input::MIRInput &input = file;
+        mir::input::MIRInput& input = file;
 
-        const mir::param::MIRParametrisation &parametrisation = input.parametrisation();
+        const mir::param::MIRParametrisation& parametrisation = input.parametrisation();
 
-        const char *sep = "";
+        const char* sep = "";
         std::string value;
 
         for (int i = 1; i < argc() - 1; i++) {
             eckit::Log::info() << sep << argv(i) << "=";
             if (parametrisation.get(argv(i), value)) {
                 eckit::Log::info() << value;
-            } else {
+            }
+            else {
                 eckit::Log::info() << "<not found>";
             }
             sep = ", ";
@@ -79,8 +76,7 @@ void MIRInspect::execute(const eckit::option::CmdArgs&) {
 }
 
 
-int main(int argc, char **argv) {
+int main(int argc, char** argv) {
     MIRInspect tool(argc, argv);
     return tool.start();
 }
-
--- src/tools/mir-make-lsm.cc
+++ src/tools/mir-make-lsm.cc
@@ -1,25 +1,25 @@
 /*
-* (C) Copyright 1996- ECMWF.
-*
-* This software is licensed under the terms of the Apache Licence Version 2.0
-* which can be obtained at http://www.apache.org/licenses/LICENSE-2.0.
-* In applying this licence, ECMWF does not waive the privileges and immunities
-* granted to it by virtue of its status as an intergovernmental organisation nor
-* does it submit to any jurisdiction.
-*/
+ * (C) Copyright 1996- ECMWF.
+ *
+ * This software is licensed under the terms of the Apache Licence Version 2.0
+ * which can be obtained at http://www.apache.org/licenses/LICENSE-2.0.
+ * In applying this licence, ECMWF does not waive the privileges and immunities
+ * granted to it by virtue of its status as an intergovernmental organisation nor
+ * does it submit to any jurisdiction.
+ */
 
 /// @author Baudouin Raoult
 /// @author Pedro Maciel
 /// @date Apr 2015
 
 
-#include "eckit/io/StdFile.h"
-#include "eckit/io/AutoCloser.h"
-#include "eckit/log/Log.h"
 #include "eckit/exception/Exceptions.h"
+#include "eckit/io/AutoCloser.h"
+#include "eckit/io/StdFile.h"
+#include "eckit/log/Log.h"
 
-#include "mir/input/GribFileInput.h"
 #include "mir/data/MIRField.h"
+#include "mir/input/GribFileInput.h"
 #include "mir/tools/MIRTool.h"
 
 class MIRMakeLSM : public mir::tools::MIRTool {
@@ -28,25 +28,20 @@ class MIRMakeLSM : public mir::tools::MIRTool {
 
     void execute(const eckit::option::CmdArgs&);
 
-    void usage(const std::string &tool) const;
+    void usage(const std::string& tool) const;
 
-    int minimumPositionalArguments() const {
-        return 2;
-    }
+    int minimumPositionalArguments() const { return 2; }
 
 public:
-
     // -- Contructors
 
-    MIRMakeLSM(int argc, char **argv) : mir::tools::MIRTool(argc, argv) {}
-
+    MIRMakeLSM(int argc, char** argv) : mir::tools::MIRTool(argc, argv) {}
 };
 
 
-void MIRMakeLSM::usage(const std::string &tool) const {
-    eckit::Log::info()
-            << "\n" << "Usage: " << tool << " file.grib file.lsm"
-            << std::endl;
+void MIRMakeLSM::usage(const std::string& tool) const {
+    eckit::Log::info() << "\n"
+                       << "Usage: " << tool << " file.grib file.lsm" << std::endl;
 }
 
 
@@ -57,9 +52,9 @@ void MIRMakeLSM::execute(const eckit::option::CmdArgs&) {
     eckit::AutoStdFile out(argv(2), "w");
 
     while (file.next()) {
-        mir::input::MIRInput &input = file;
+        mir::input::MIRInput& input = file;
 
-        const mir::param::MIRParametrisation &parametrisation = input.parametrisation();
+        const mir::param::MIRParametrisation& parametrisation = input.parametrisation();
 
         size_t Ni = 0;
         size_t Nj = 0;
@@ -67,7 +62,7 @@ void MIRMakeLSM::execute(const eckit::option::CmdArgs&) {
         ASSERT(parametrisation.get("Ni", Ni));
         ASSERT(parametrisation.get("Nj", Nj));
 
-        eckit::Log::info() << "Ni=" << Ni << ", Nj=" << Nj << ", size=" << Ni*Nj << std::endl;
+        eckit::Log::info() << "Ni=" << Ni << ", Nj=" << Nj << ", size=" << Ni * Nj << std::endl;
         ASSERT(Ni == Nj * 2);
 
 
@@ -76,7 +71,7 @@ void MIRMakeLSM::execute(const eckit::option::CmdArgs&) {
         ASSERT(!field.hasMissing());
 
         unsigned char byte = 0;
-        size_t n = 0;
+        size_t n           = 0;
         for (const auto& v : field.values(0)) {
             bool land = v >= 0.5;
             byte <<= 1;
@@ -90,22 +85,18 @@ void MIRMakeLSM::execute(const eckit::option::CmdArgs&) {
             }
         }
 
-        ASSERT(n == 0); // Reader will workout Ni Nj from file size
+        ASSERT(n == 0);  // Reader will workout Ni Nj from file size
 
         // if (n) {
         //     byte <<= (8 - n);
         //     ASSERT(fwrite(&byte, 1, 1, out) == 1);
 
         // }
-
     }
-
 }
 
 
-int main(int argc, char **argv) {
+int main(int argc, char** argv) {
     MIRMakeLSM tool(argc, argv);
     return tool.start();
 }
-
-
