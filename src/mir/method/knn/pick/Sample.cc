d073b67fd8f5488ed7fee390d900bb32f889fe36
Pedro Maciel <Pedro.Maciel@ecmwf.int>
2019-12-13 16:36:25 +0000

    MIR-426: k-nearest neighbours sampling for large search radii

---
+++ src/mir/method/knn/pick/Sample.cc
@@ -0,0 +1,90 @@
+/*
+ * (C) Copyright 1996- ECMWF.
+ *
+ * This software is licensed under the terms of the Apache Licence Version 2.0
+ * which can be obtained at http://www.apache.org/licenses/LICENSE-2.0.
+ * In applying this licence, ECMWF does not waive the privileges and immunities
+ * granted to it by virtue of its status as an intergovernmental organisation nor
+ * does it submit to any jurisdiction.
+ */
+
+
+#include "mir/method/knn/pick/Sample.h"
+
+#include <cstdlib>
+
+#include "eckit/exception/Exceptions.h"
+#include "eckit/types/FloatCompare.h"
+#include "eckit/utils/MD5.h"
+
+#include "mir/param/MIRParametrisation.h"
+
+
+namespace mir {
+namespace method {
+namespace knn {
+namespace pick {
+
+
+Sample::Sample(const param::MIRParametrisation& param) {
+    distance_ = 1.;
+    param.get("distance", distance_);
+    ASSERT(distance_ > 0.);
+
+    nClosest_ = 4;
+    param.get("nclosest", nClosest_);
+    ASSERT(nClosest_ > 0);
+}
+
+
+void Sample::pick(const search::PointSearch& tree, const eckit::geometry::Point3& p,
+                  Pick::neighbours_t& closest) const {
+    tree.closestWithinRadius(p, distance_, closest);
+    if (closest.size() <= nClosest_) {
+        return;
+    }
+
+    // reservoir sampling in-place (output not ordered by distance)
+    for (size_t n = nClosest_; n < closest.size(); ++n) {
+        auto r = static_cast<size_t>(std::rand() % n);
+        if (r < nClosest_) {
+            closest[r] = closest[n];
+        }
+    }
+
+    // closest.resize(nClosest_);  // FIXME: better than below (but has black magic)
+    Pick::neighbours_t sample(closest.begin(), closest.begin() + nClosest_);
+    closest.swap(sample);
+}
+
+
+size_t Sample::n() const {
+    return nClosest_;
+}
+
+
+bool Sample::sameAs(const Pick& other) const {
+    auto o = dynamic_cast<const Sample*>(&other);
+    return o && eckit::types::is_approximately_equal(distance_, o->distance_);
+}
+
+
+void Sample::print(std::ostream& out) const {
+    out << "Sample[nclosest=" << nClosest_ << ",distance=" << distance_ << "]";
+}
+
+
+void Sample::hash(eckit::MD5& h) const {
+    h << "sample";
+    h << nClosest_;
+    h << distance_;
+}
+
+
+static PickBuilder<Sample> __pick("sample");
+
+
+}  // namespace pick
+}  // namespace knn
+}  // namespace method
+}  // namespace mir
---
+++ src/mir/method/knn/pick/SortedSample.cc
@@ -0,0 +1,64 @@
+/*
+ * (C) Copyright 1996- ECMWF.
+ *
+ * This software is licensed under the terms of the Apache Licence Version 2.0
+ * which can be obtained at http://www.apache.org/licenses/LICENSE-2.0.
+ * In applying this licence, ECMWF does not waive the privileges and immunities
+ * granted to it by virtue of its status as an intergovernmental organisation nor
+ * does it submit to any jurisdiction.
+ */
+
+
+#include "mir/method/knn/pick/SortedSample.h"
+
+#include <algorithm>
+
+#include "eckit/utils/MD5.h"
+
+
+namespace mir {
+namespace method {
+namespace knn {
+namespace pick {
+
+
+SortedSample::SortedSample(const param::MIRParametrisation& param) : sample_(param) {}
+
+
+void SortedSample::pick(const search::PointSearch& tree, const eckit::geometry::Point3& p,
+                        Pick::neighbours_t& closest) const {
+    sample_.pick(tree, p, closest);
+
+    std::sort(closest.begin(), closest.end(),
+              [&p](const Pick::neighbours_t::value_type& a, const Pick::neighbours_t::value_type& b) {
+                  return Point3::distance2(a.point(), p) < Point3::distance2(b.point(), p);
+              });
+}
+
+size_t SortedSample::n() const { return sample_.n(); }
+
+
+bool SortedSample::sameAs(const Pick& other) const {
+    auto o = dynamic_cast<const SortedSample*>(&other);
+    return o && sample_.sameAs(*o);
+}
+
+
+void SortedSample::hash(eckit::MD5& h) const {
+    h << "sample-sorted";
+    h << sample_;
+}
+
+
+void SortedSample::print(std::ostream& out) const {
+    out << "SortedSample[sample=" << sample_ << "]";
+}
+
+
+static PickBuilder<SortedSample> __pick("sorted-sample");
+
+
+}  // namespace pick
+}  // namespace knn
+}  // namespace method
+}  // namespace mir
