f70739bb573270be632a372a8f308d2a06ac728f
Pedro Maciel <Pedro.Maciel@ecmwf.int>
2020-09-11 19:15:29 +0100

    MIR-482 unit tests fix

--- src/mir/packing/IEEE.cc
+++ src/mir/packing/IEEE.cc
@@ -13,7 +13,11 @@
 #include "mir/packing/IEEE.h"
 
 #include <iostream>
+#include <sstream>
 
+#include "eckit/exception/Exceptions.h"
+
+#include "mir/param/MIRParametrisation.h"
 #include "mir/util/Grib.h"
 
 
@@ -24,7 +28,17 @@ namespace packing {
 static PackerBuilder<IEEE> __packer("ieee");
 
 
-IEEE::IEEE(const param::MIRParametrisation& param) : Packer(param) {}
+IEEE::IEEE(const param::MIRParametrisation& param) : Packer(param) {
+    long bits = -1;
+    ASSERT(param.userParametrisation().get("accuracy", bits) || param.fieldParametrisation().get("accuracy", bits));
+
+    if (bits != 32 && bits != 64) {
+        std::ostringstream msg;
+        msg << *this << ": only supports bitsPerValue 32 and 64 (from input, or as set by 'accuracy')";
+        eckit::Log::error() << msg.str() << std::endl;
+        throw eckit::UserError(msg.str());
+    }
+}
 
 
 IEEE::~IEEE() = default;
