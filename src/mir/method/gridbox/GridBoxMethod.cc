ecmwf/mir

commit a2025944fad416fd31cec8f0d1753704662d4bb9

ATLAS-269, MIR-465: grid-box average interpolation small performance improvement

src/mir/method/gridbox/GridBoxMethod.cc
@@ -137,6 +137,7 @@ void GridBoxMethod::assemble(util::MIRStatistics&, WeightMatrix& W, const repres
             ASSERT(area > 0.);
 
             double sumSmallAreas = 0.;
+            bool areaMatch       = false;
             for (auto& c : closest) {
                 auto j        = c.payload();
                 auto smallBox = inBoxes.at(j);
@@ -147,12 +148,16 @@ void GridBoxMethod::assemble(util::MIRStatistics&, WeightMatrix& W, const repres
 
                     triplets.emplace_back(WeightMatrix::Triplet(i, j, smallArea / area));
                     sumSmallAreas += smallArea;
+
+                    if ( ( areaMatch = eckit::types::is_approximately_equal( area, sumSmallAreas, 1. /*m^2*/ ) ) ) {
+                        break;
+                    }
                 }
             }
 
 
             // insert the interpolant weights into the global (sparse) interpolant matrix
-            if (!triplets.empty() && eckit::types::is_approximately_equal(area, sumSmallAreas, 1. /*m^2*/)) {
+            if (!areaMatch) {
                 std::copy(triplets.begin(), triplets.end(), std::back_inserter(weights_triplets));
             }
             else {

commit 76764a0a722f17331e15da99151bcf5e0e3c22f9

ATLAS-269, MIR-465 Fix bug introduced in commit a2025944

src/mir/method/gridbox/GridBoxMethod.cc
@@ -157,7 +157,7 @@ void GridBoxMethod::assemble(util::MIRStatistics&, WeightMatrix& W, const repres
 
 
             // insert the interpolant weights into the global (sparse) interpolant matrix
-            if (!areaMatch) {
+            if (areaMatch) {
                 std::copy(triplets.begin(), triplets.end(), std::back_inserter(weights_triplets));
             }
             else {