02b8f62745d8d5edb67ab61c0d711f5c0dd61bcb
Olivier Iffrig <olivier.iffrig@ecmwf.int>
2021-05-07 15:08:35 +0100

    Allow overriding the include/library paths

--- setup.py
+++ setup.py
@@ -1,7 +1,18 @@
 from distutils.core import setup
 from distutils.extension import Extension
+import os
 from Cython.Build import cythonize
 from Cython.Distutils import build_ext
+
+mir_lib_dir_default = "/var/tmp/mafr/builds/debug/mir/lib"
+mir_include_dirs_default = ["/home/ma/mafr/dev/mir/src", "/var/tmp/mafr/builds/debug/mir/src",
+                            "/home/ma/mafr/dev/eckit/src", "/var/tmp/mafr/builds/debug/eckit/src"]
+
+mir_lib_dir = os.environ.get("MIR_LIB_DIR", mir_lib_dir_default)
+mir_include_dirs = os.environ.get("MIR_INCLUDE_DIRS", ":".join(mir_include_dirs_default)).split(":")
+
+print("MIR lib dir:", mir_lib_dir)
+print("MIR include dirs:", " ".join(mir_include_dirs))
 
 # FIXME: sort out how to discover MIR libraries
 setup(
@@ -12,10 +23,9 @@ setup(
             ["mir.pyx"],
             language = "c++",
             libraries = ["mir"],
-            library_dirs = ["/var/tmp/mafr/builds/debug/mir/lib"],
-            runtime_library_dirs = ["/var/tmp/mafr/builds/debug/mir/lib"],
-            include_dirs = ["/home/ma/mafr/dev/mir/src", "/var/tmp/mafr/builds/debug/mir/src",
-                            "/home/ma/mafr/dev/eckit/src", "/var/tmp/mafr/builds/debug/eckit/src"],
+            library_dirs = [mir_lib_dir],
+            runtime_library_dirs = [mir_lib_dir],
+            include_dirs = mir_include_dirs,
             extra_compile_args = ["-std=c++11"],
             extra_link_args = ["-std=c++11"],
             cmdclass = {'build_ext': build_ext}
