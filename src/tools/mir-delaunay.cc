7c4975879b789170dff4d741ad3000a07ac03774
Pedro Maciel <Pedro.Maciel@ecmwf.int>
2021-09-21 17:14:51 +0100

    Qhull

--- src/tools/mir-delaunay.cc
+++ src/tools/mir-delaunay.cc
@@ -14,6 +14,8 @@ extern "C" {
 #include "libqhull_r/qhull_ra.h"
 }
 
+#include <fstream>
+#include <iostream>
 #include <memory>
 #include <string>
 #include <vector>
@@ -62,37 +64,98 @@ void MIRDelaunay::execute(const eckit::option::CmdArgs& args) {
             Log::info() << "#" << ++j << ": " << *repres << std::endl;
 
             auto nbPoints = repres->numberOfPoints();
+            auto points   = new coordT[3 * nbPoints];  //  Qhull-handled
 
-            std::vector<Point3> points;
-            points.reserve(nbPoints);
-
-            for (std::unique_ptr<repres::Iterator> iterator(repres->iterator()); iterator->next();) {
-                points.emplace_back(iterator->point3D());
+            {
+                auto pp = &points[0];
+                for (std::unique_ptr<repres::Iterator> iterator(repres->iterator()); iterator->next();) {
+                    auto p = iterator->point3D();
+                    *pp++  = p[XYZCOORDS::XX];
+                    *pp++  = p[XYZCOORDS::YY];
+                    *pp++  = p[XYZCOORDS::ZZ];
+                }
             }
 
             // Qhull's data structure
-            qhT qh_qh;
-            qhT* qh = &qh_qh;
+            qhT qh;
 
             QHULL_LIB_CHECK
 
-            qh_zero(qh, stderr);
+            qh_zero(&qh, stderr);
 
-            const char* flags_convexhull = "qhull s Tcv";
-            const char* flags_delaunay   = "qhull s d Tcv";
+            auto flags = const_cast<char*>("qhull Qt");
             // For a Voronoi diagram ('v') call qh_setvoronoi_all() after qh_new_qhull()
 
-            ASSERT(0 == qh_new_qhull(qh, 3, int(nbPoints), reinterpret_cast<coordT*>(points.data()), true,
-                                     const_cast<char*>(flags_convexhull), nullptr, stderr));
+            ASSERT(0 == qh_new_qhull(&qh, 3, int(nbPoints), points, true, flags, stdout, stderr));
 
-            Log::info() << qh->num_vertices << " vertices and " << qh->num_facets << " facets" << std::endl;
-            for (auto facet = qh->facet_list; (facet != nullptr) && (facet->next != nullptr); facet = facet->next) {
+
+            std::ofstream f("out.msh");
+            f << "$MeshFormat"
+                 "\n2.0 0 8"
+                 "\n$EndMeshFormat";
+
+            {
+                f << "\n$Nodes"
+                     "\n"
+                  << nbPoints;
+
+#if 1
+                auto pp = &points[0];
+                for (size_t i = 1; i <= nbPoints; ++i) {
+                    auto x = pp++;
+                    auto y = pp++;
+                    auto z = pp++;
+                    f << '\n' << i << ' ' << *x << ' ' << *y << ' ' << *z;
+                }
+#else
+                for (auto vx = qh.vertex_list; (vx != nullptr) && (vx->next != nullptr); vx = vx->next) {
+                    auto p = vx->point;
+                    f << '\n' << i++ << ' ' << p[XYZCOORDS::XX] << ' ' << p[XYZCOORDS::YY] << ' ' << p[XYZCOORDS::ZZ];
+                }
+#endif
+                f << "\n$EndNodes";
             }
 
+            {
+                f << "\n$Elements"
+                     "\n"
+                  << qh.num_facets;
 
-            qh->NOerrexit = True;
+                size_t i = 1;
+                for (auto facet = qh.facet_list; (facet != nullptr) && (facet->next != nullptr); facet = facet->next) {
+                    std::vector<unsigned int> vertex;
+                    vertex.reserve(3);
 
-            qh_freeqhull(qh, qh_ALL);
+                    for (vertexT *v, **vp = reinterpret_cast<vertexT**>(&(facet->vertices->e[0].p));
+                         (v = *vp++) != nullptr && vertex.size() <= 3;) {
+                        vertex.emplace_back(v->id);
+                    }
+
+                    ASSERT(vertex.size() == 3);
+                    f << "\n" << i++ << " 2 1 99 " << vertex[0] << ' ' << vertex[1] << ' ' << vertex[2];
+                }
+                f << "\n$EndElements";
+            }
+
+            f << "\n";
+
+
+            Log::info() << qh.num_vertices << " vertices and " << qh.num_facets << " facets" << std::endl;
+            //            for (auto facet = qh.facet_list; (facet != nullptr) && (facet->next != nullptr); facet =
+            //            facet->next) {
+
+            //                Log::info() << "x" << std::endl;
+            //                for (vertexT *vertex = nullptr, **vertexp =
+            //                reinterpret_cast<vertexT**>(&(facet->vertices->e[0].p));
+            //                     (vertex = *vertexp++);) {
+            //                    Log::info() << "v: " << vertex->point << " " << vertex->id << std::endl;
+            //                }
+            //            }
+
+
+            qh.NOerrexit = True;
+
+            qh_freeqhull(&qh, qh_ALL);
         }
     }
 
