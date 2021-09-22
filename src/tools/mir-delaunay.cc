9f8cb95191084329580eb1635900d2b981c07b0f
Pedro Maciel <Pedro.Maciel@ecmwf.int>
2021-09-22 09:09:15 +0100

    Qhull

--- src/tools/mir-delaunay.cc
+++ src/tools/mir-delaunay.cc
@@ -10,15 +10,15 @@
  */
 
 
-extern "C" {
-#include "libqhull_r/qhull_ra.h"
-}
-
 #include <fstream>
-#include <iostream>
 #include <memory>
-#include <string>
 #include <vector>
+
+#include "libqhullcpp/Qhull.h"
+#include "libqhullcpp/QhullFacetList.h"
+#include "libqhullcpp/QhullFacetSet.h"
+#include "libqhullcpp/QhullVertexSet.h"
+#include "libqhullcpp/RboxPoints.h"
 
 #include "eckit/option/CmdArgs.h"
 #include "eckit/option/SimpleOption.h"
@@ -63,99 +63,175 @@ void MIRDelaunay::execute(const eckit::option::CmdArgs& args) {
 
             Log::info() << "#" << ++j << ": " << *repres << std::endl;
 
-            auto nbPoints = repres->numberOfPoints();
-            auto points   = new coordT[3 * nbPoints];  //  Qhull-handled
+            using orgQhull::Qhull;
+            using orgQhull::RboxPoints;
 
+            enum
             {
-                auto pp = &points[0];
+                EG_CONVEX,
+                EG_DELAUNAY,
+                EG_VORONOI
+            } mode = EG_CONVEX;
+
+            if (mode == EG_CONVEX) {
+                RboxPoints rbox;
+                rbox.setDimension(3);
                 for (std::unique_ptr<repres::Iterator> iterator(repres->iterator()); iterator->next();) {
                     auto p = iterator->point3D();
-                    *pp++  = p[XYZCOORDS::XX];
-                    *pp++  = p[XYZCOORDS::YY];
-                    *pp++  = p[XYZCOORDS::ZZ];
+                    rbox.append(3, p.data());
                 }
-            }
 
-            // Qhull's data structure
-            qhT qh;
+                Qhull q(rbox, "s o Qt");
+                q.setOutputStream(&Log::info());
+                q.setErrorStream(&Log::error());
 
-            QHULL_LIB_CHECK
+                // q.outputQhull();
 
-            qh_zero(&qh, stderr);
+                if (true) {
+                    std::ofstream f("out.msh");
+                    f << "$MeshFormat"
+                         "\n2.0 0 8"
+                         "\n$EndMeshFormat";
 
-            auto flags = const_cast<char*>("qhull Qt");
-            // For a Voronoi diagram ('v') call qh_setvoronoi_all() after qh_new_qhull()
+                    {
+                        f << "\n$Nodes"
+                             "\n"
+                          << rbox.count();
 
-            ASSERT(0 == qh_new_qhull(&qh, 3, int(nbPoints), points, true, flags, stdout, stderr));
-
-
-            std::ofstream f("out.msh");
-            f << "$MeshFormat"
-                 "\n2.0 0 8"
-                 "\n$EndMeshFormat";
-
-            {
-                f << "\n$Nodes"
-                     "\n"
-                  << nbPoints;
-
-#if 1
-                auto pp = &points[0];
-                for (size_t i = 1; i <= nbPoints; ++i) {
-                    auto x = pp++;
-                    auto y = pp++;
-                    auto z = pp++;
-                    f << '\n' << i << ' ' << *x << ' ' << *y << ' ' << *z;
-                }
-#else
-                for (auto vx = qh.vertex_list; (vx != nullptr) && (vx->next != nullptr); vx = vx->next) {
-                    auto p = vx->point;
-                    f << '\n' << i++ << ' ' << p[XYZCOORDS::XX] << ' ' << p[XYZCOORDS::YY] << ' ' << p[XYZCOORDS::ZZ];
-                }
-#endif
-                f << "\n$EndNodes";
-            }
-
-            {
-                f << "\n$Elements"
-                     "\n"
-                  << qh.num_facets;
-
-                size_t i = 1;
-                for (auto facet = qh.facet_list; (facet != nullptr) && (facet->next != nullptr); facet = facet->next) {
-                    std::vector<unsigned int> vertex;
-                    vertex.reserve(3);
-
-                    for (vertexT *v, **vp = reinterpret_cast<vertexT**>(&(facet->vertices->e[0].p));
-                         (v = *vp++) != nullptr && vertex.size() <= 3;) {
-                        vertex.emplace_back(v->id);
+                        size_t i = 1;
+                        for (auto& p : rbox) {
+                            f << '\n'
+                              << i++ << ' ' << p[XYZCOORDS::XX] << ' ' << p[XYZCOORDS::YY] << ' ' << p[XYZCOORDS::ZZ];
+                        }
+                        f << "\n$EndNodes";
                     }
 
-                    ASSERT(vertex.size() == 3);
-                    f << "\n" << i++ << " 2 1 99 " << vertex[0] << ' ' << vertex[1] << ' ' << vertex[2];
+                    size_t isTopOrient = 0;
+                    {
+                        f << "\n$Elements"
+                             "\n"
+                          << q.facetCount();
+
+                        size_t i = 1;
+                        for (const auto& ft : q.facetList()) {
+                            ASSERT(ft.isGood());
+
+                            auto x = [](const orgQhull::QhullVertex& v) { return v.point().id() + 1; };
+                            auto v = ft.vertices();
+                            ASSERT(v.count() == 3);
+
+                            if (ft.isTopOrient()) {
+                                isTopOrient++;
+                                f << '\n' << i++ << " 2 1 99 " << x(v[0]) << ' ' << x(v[1]) << ' ' << x(v[2]);
+                            }
+                            else {
+                                f << '\n' << i++ << " 2 1 99 " << x(v[1]) << ' ' << x(v[0]) << ' ' << x(v[2]);
+                            }
+                        }
+
+                        f << "\n$EndElements";
+                    }
+
+                    f << "\n";
                 }
-                f << "\n$EndElements";
             }
 
-            f << "\n";
+            if (mode == EG_DELAUNAY) {
+                RboxPoints rbox("y c D2");
+                Log::info() << rbox;
 
+                Qhull q(rbox, "d o");
+                q.setOutputStream(&Log::info());
+                q.outputQhull();
+                // q.outputQhull("o") produces the same output
 
-            Log::info() << qh.num_vertices << " vertices and " << qh.num_facets << " facets" << std::endl;
-            //            for (auto facet = qh.facet_list; (facet != nullptr) && (facet->next != nullptr); facet =
-            //            facet->next) {
+                Qhull qhull;
+                qhull.setOutputStream(&Log::info());
 
-            //                Log::info() << "x" << std::endl;
-            //                for (vertexT *vertex = nullptr, **vertexp =
-            //                reinterpret_cast<vertexT**>(&(facet->vertices->e[0].p));
-            //                     (vertex = *vertexp++);) {
-            //                    Log::info() << "v: " << vertex->point << " " << vertex->id << std::endl;
-            //                }
-            //            }
+                // The Delaunay diagram is equivalent to the convex hull of a paraboloid, one dimension higher
 
+                // Input sites as a vector of vectors
+                std::vector<std::vector<double> > inputSites;
+                for (auto& point : qhull.points()) {
+                    inputSites.push_back(point.toStdVector());
+                }
 
-            qh.NOerrexit = True;
+                // Delaunay regions as a vector of vectors
+                std::vector<std::vector<int> > regions;
+                for (auto& f : qhull.facetList()) {
+                    std::vector<int> vertices;
+                    if (!f.isUpperDelaunay()) {
+                        if (!f.isTopOrient() && f.isSimplicial()) { /* orient the vertices like option 'o' */
+                            auto vs = f.vertices();
+                            vertices.push_back(vs[1].point().id());
+                            vertices.push_back(vs[0].point().id());
+                            for (int i = 2; i < (int)vs.size(); ++i) {
+                                vertices.push_back(vs[i].point().id());
+                            }
+                        }
+                        else { /* note: for non-simplicial facets, this code does not duplicate option 'o', see
+                                  qh_facet3vertex and qh_printfacetNvertex_nonsimplicial */
+                            for (auto vertex : f.vertices()) {
+                                vertices.push_back(vertex.point().id());
+                            }
+                        }
+                        regions.push_back(vertices);
+                    }
+                }
+            }
 
-            qh_freeqhull(&qh, qh_ALL);
+            if (mode == EG_VORONOI) {
+                RboxPoints rbox("y c D2");
+                Log::info() << rbox;
+
+                Qhull q(rbox, "v o");
+                q.setOutputStream(&Log::info());
+                q.outputQhull();
+                // q.outputQhull("o") produces the same output
+                bool isLower;
+                int voronoiVertexCount;
+                q.prepareVoronoi(&isLower, &voronoiVertexCount);
+
+                Qhull qhull;
+                qhull.setOutputStream(&Log::info());
+
+                // Voronoi vertex at infinity is not included
+                int numfacets    = qhull.facetCount();
+                size_t numpoints = qhull.points().size();
+
+                // Gather Voronoi vertices
+                std::vector<std::vector<double> > voronoiVertices;
+                for (auto& facet : qhull.facetList()) {
+                    if (facet.visitId() && facet.visitId() < numfacets) {
+                        voronoiVertices.push_back(facet.getCenter().toStdVector());
+                    }
+                }
+
+                // Gather Voronoi regions (qh_printvoronoi calls qh_pointvertex via qh_markvoronoi)
+                std::vector<std::vector<int> > voronoiRegions(numpoints);
+                for (auto& vertex : qhull.vertexList()) {
+                    size_t numinf = 0;
+                    std::vector<int> voronoiRegion;
+                    for (auto neighbor : vertex.neighborFacets()) {
+                        if (neighbor.visitId() == 0) {
+                            if (numinf) {
+                                numinf = 1;
+                                voronoiRegion.push_back(-1);  // -1 indicates the Voronoi vertex at infinity
+                            }
+                        }
+                        else if (neighbor.visitId() < numfacets) {
+                            voronoiRegion.push_back(neighbor.visitId() - 1);
+                        }
+                    }
+
+                    if (voronoiRegion.size() > numinf) {
+                        int siteId = vertex.point().id();
+                        if (0 <= siteId && siteId < int(numpoints)) {  // otherwise would indicate qh.other_points
+                            voronoiRegions[siteId] = voronoiRegion;
+                        }
+                    }
+                }
+            }
         }
     }
 
