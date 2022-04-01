2042b5cb5cfb153bb0ee63bf350e3cf7564cc9a0
Pedro Maciel <Pedro.Maciel@ecmwf.int>
2022-04-01 09:47:06 +0100

    Improved defaults

--- setup.py
+++ setup.py
@@ -1,34 +1,48 @@
+import os
 from distutils.core import setup
 from distutils.extension import Extension
-import os
+
 from Cython.Build import cythonize
 from Cython.Distutils import build_ext
 
-mir_lib_dir_default = "/var/tmp/mafr/builds/debug/mir/lib"
-mir_include_dirs_default = ["/home/ma/mafr/dev/mir/src", "/var/tmp/mafr/builds/debug/mir/src",
-                            "/home/ma/mafr/dev/eckit/src", "/var/tmp/mafr/builds/debug/eckit/src"]
+home = os.environ.get("HOME")
+source = os.path.join(home, "git", "mir-bundle")
+build = os.path.join(home, "build", "mir-bundle")
 
-mir_lib_dir = os.environ.get("MIR_LIB_DIR", mir_lib_dir_default)
-mir_include_dirs = os.environ.get("MIR_INCLUDE_DIRS", ":".join(mir_include_dirs_default)).split(":")
+library_dirs = (
+    os.environ.get("MIR_LIB_DIR").split(":")
+    if "MIR_LIB_DIR" in os.environ
+    else [os.path.join(build, "lib")]
+)
 
-print("MIR lib dir:", mir_lib_dir)
-print("MIR include dirs:", " ".join(mir_include_dirs))
+include_dirs = (
+    os.environ.get("MIR_INCLUDE_DIRS").split(":")
+    if "MIR_INCLUDE_DIRS" in os.environ
+    else [
+        os.path.join(base, pkg, "src")
+        for base in [source, build]
+        for pkg in ["mir", "eckit", "eccodes"]
+    ]
+)
+
+print("library_dirs:", " ".join(library_dirs))
+print("include_dirs:", " ".join(include_dirs))
 
 # FIXME: sort out how to discover MIR libraries
 setup(
-    name = 'mir',
-    ext_modules = cythonize(
+    name="mir",
+    ext_modules=cythonize(
         Extension(
             "mir",
             ["mir.pyx", "pyio.cc"],
-            language = "c++",
-            libraries = ["mir"],
-            library_dirs = [mir_lib_dir],
-            runtime_library_dirs = [mir_lib_dir],
-            include_dirs = mir_include_dirs,
-            extra_compile_args = ["-std=c++11"],
-            extra_link_args = ["-std=c++11"],
+            language="c++",
+            libraries=["mir"],
+            library_dirs=library_dirs,
+            runtime_library_dirs=library_dirs,
+            include_dirs=include_dirs,
+            extra_compile_args=["-std=c++11"],
+            extra_link_args=["-std=c++11"],
         ),
-        compiler_directives={'language_level': 3, 'c_string_encoding': 'default'}
-    )
+        compiler_directives={"language_level": 3, "c_string_encoding": "default"},
+    ),
 )
