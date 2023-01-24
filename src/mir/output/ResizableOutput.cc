1c38200bb8294c2f02cc47dabda85079ced29a48
Pedro Maciel <Pedro.Maciel@ecmwf.int>
2023-01-24 09:27:19 +0000

    MUL-70: ResizableOutput taking in a std::vector<double>& that is resized on output (copy/save) for MultIO

---
+++ src/mir/output/ResizableOutput.cc
@@ -0,0 +1,89 @@
+/*
+ * (C) Copyright 1996- ECMWF.
+ *
+ * This software is licensed under the terms of the Apache Licence Version 2.0
+ * which can be obtained at http://www.apache.org/licenses/LICENSE-2.0.
+ *
+ * In applying this licence, ECMWF does not waive the privileges and immunities
+ * granted to it by virtue of its status as an intergovernmental organisation nor
+ * does it submit to any jurisdiction.
+ */
+
+
+#include "mir/output/ResizableOutput.h"
+
+#include <ostream>
+
+#include "mir/action/context/Context.h"
+#include "mir/api/MIRJob.h"
+#include "mir/data/MIRField.h"
+#include "mir/repres/Representation.h"
+#include "mir/util/Exceptions.h"
+#include "mir/util/Log.h"
+
+
+namespace mir {
+namespace output {
+
+
+ResizableOutput::ResizableOutput(std::vector<double>& values, param::SimpleParametrisation& metadata) :
+    values_(values), metadata_(metadata) {}
+
+
+size_t ResizableOutput::save(const param::MIRParametrisation& /*param*/, context::Context& ctx) {
+    const auto& field = ctx.field();
+    field.validate();
+
+
+    // save metadata
+    {
+        Log::debug() << "ResizableOutput::save metadata" << std::endl;
+        repres::RepresentationHandle repres(field.representation());
+
+        // (a hack)
+        api::MIRJob job;
+        repres->fillJob(job);
+        job.copyValuesTo(metadata_);
+
+        if (field.hasMissing()) {
+            metadata_.set("missing_value", field.missingValue());
+        }
+    }
+
+
+    // save data
+    ASSERT(field.dimensions() == 1);
+    values_ = field.values(0);
+
+    return values_.size() * sizeof(double);
+}
+
+
+bool ResizableOutput::sameAs(const MIROutput& other) const {
+    return dynamic_cast<const MIROutput*>(this) == &other;
+}
+
+
+bool ResizableOutput::sameParametrisation(const param::MIRParametrisation& /*unused*/,
+                                          const param::MIRParametrisation& /*unused*/) const {
+    return true;
+}
+
+
+bool ResizableOutput::printParametrisation(std::ostream& /*out*/, const param::MIRParametrisation& /*param*/) const {
+    return false;
+}
+
+
+void ResizableOutput::print(std::ostream& out) const {
+    out << "ResizableOutput[size=" << size() << "]";
+}
+
+
+size_t ResizableOutput::size() const {
+    return values_.size();
+}
+
+
+}  // namespace output
+}  // namespace mir
---
+++ src/mir/output/ResizableOutput.h
@@ -0,0 +1,86 @@
+/*
+ * (C) Copyright 1996- ECMWF.
+ *
+ * This software is licensed under the terms of the Apache Licence Version 2.0
+ * which can be obtained at http://www.apache.org/licenses/LICENSE-2.0.
+ *
+ * In applying this licence, ECMWF does not waive the privileges and immunities
+ * granted to it by virtue of its status as an intergovernmental organisation nor
+ * does it submit to any jurisdiction.
+ */
+
+
+#pragma once
+
+#include <vector>
+
+#include "mir/output/MIROutput.h"
+#include "mir/param/SimpleParametrisation.h"
+
+
+namespace mir {
+namespace output {
+
+
+class ResizableOutput final : public MIROutput {
+public:
+    // -- Exceptions
+    // None
+
+    // -- Constructors
+
+    ResizableOutput(std::vector<double>&, param::SimpleParametrisation& metadata);
+
+    // -- Destructor
+    // None
+
+    // -- Convertors
+    // None
+
+    // -- Operators
+    // None
+
+    // -- Methods
+
+    size_t size() const;
+
+    // -- Overridden methods
+    // None
+
+    // -- Class members
+    // None
+
+    // -- Class methods
+    // None
+
+private:
+    // -- Members
+
+    std::vector<double>& values_;
+    param::SimpleParametrisation& metadata_;
+
+    // -- Methods
+    // None
+
+    // -- Overridden methods
+
+    // From MIROutput
+    size_t save(const param::MIRParametrisation&, context::Context&) override;
+    bool sameAs(const MIROutput&) const override;
+    bool sameParametrisation(const param::MIRParametrisation&, const param::MIRParametrisation&) const override;
+    bool printParametrisation(std::ostream&, const param::MIRParametrisation&) const override;
+    void print(std::ostream&) const override;
+
+    // -- Class members
+    // None
+
+    // -- Class methods
+    // None
+
+    // -- Friends
+    // None
+};
+
+
+}  // namespace output
+}  // namespace mir
