85a06f2e9e0f68cdfd92e985aeac391918389ded
Pedro Maciel <Pedro.Maciel@ecmwf.int>
2023-02-06 15:55:29 +0000

    MIR-592 Wind vector components at the poles for regular lat/lon grids, Finite Element infinitesimal displacement at the pole, restrict to grids with domains containing a/the poles (performance)

--- src/mir/repres/Representation.h
+++ src/mir/repres/Representation.h
@@ -111,10 +111,13 @@ public:
 
     virtual atlas::Grid atlasGrid() const;
 
+    // Domain operations
     virtual util::Domain domain() const;
     virtual const util::BoundingBox& boundingBox() const;
-
     virtual bool isGlobal() const;
+    virtual bool isPeriodicWestEast() const;
+    virtual bool includesNorthPole() const;
+    virtual bool includesSouthPole() const;
 
     virtual size_t truncation() const;
 
@@ -147,11 +150,6 @@ protected:
 
     virtual void print(std::ostream&) const = 0;
     virtual void makeName(std::ostream&) const;
-
-    // Domain operations
-    virtual bool isPeriodicWestEast() const;
-    virtual bool includesNorthPole() const;
-    virtual bool includesSouthPole() const;
 
     // -- Overridden methods
     // None
