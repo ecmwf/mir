fab715b180272f5d487b247a357b19a1a36a043a
Pedro Maciel <Pedro.Maciel@ecmwf.int>
2023-02-15 12:06:12 +0000

    MIR-598 Improved interface to compute/--formula, test tool

--- src/tools/mir-compute.cc
+++ src/tools/mir-compute.cc
@@ -12,10 +12,11 @@
 
 #include <memory>
 #include <sstream>
+#include <vector>
 
 #include "eckit/option/CmdArgs.h"
 #include "eckit/option/SimpleOption.h"
-#include "eckit/utils/StringTools.h"
+#include "eckit/option/VectorOption.h"
 
 #include "mir/action/calc/FormulaAction.h"
 #include "mir/action/context/Context.h"
@@ -36,11 +37,10 @@ namespace tools {
 
 struct MIRCompute : MIRTool {
     MIRCompute(int argc, char** argv) : MIRTool(argc, argv) {
-        using str = eckit::option::SimpleOption<std::string>;
-
-        options_.push_back(new str("input", "Input options (YAML)"));
-        options_.push_back(new str("formula", "Formula using variables 'f'/'f1'/'f2'/..."));
-        options_.push_back(new str("metadata", "Set eccodes keys to integer values (a=b,c=d,..)"));
+        options_.push_back(new eckit::option::SimpleOption<std::string>("input", "Input options (YAML)"));
+        options_.push_back(new eckit::option::VectorOption<std::string>(
+            "formula", "Formula(s) on variables f/f1/f2/... (formula;formula;...)", 0, ";"));
+        options_.push_back(new eckit::option::VectorOption<long>("param-id", "GRIB paramId(s) (1;2;...)", 0, ";"));
     }
 
     int numberOfPositionalArguments() const override { return 2; }
@@ -49,7 +49,8 @@ struct MIRCompute : MIRTool {
         Log::info() << "\n"
                        "Usage: "
                     << tool
-                    << " [--input={}] [--formula=f(f,f1,f2,...)] [--metadata=a=1,b=2,...)] input.grib output.grib"
+                    << " --formula=f(f,f1,f2,...)[;g(f,f1,f2,...)[;...]] [--param-id=1[;2[;...]]] [--input={}] "
+                       "input.grib output.grib"
                        "\n"
                        "Examples: "
                        "\n  % "
@@ -58,7 +59,7 @@ struct MIRCompute : MIRTool {
                        "\n  % "
                     << tool
                     << " --formula=\"sqrt(f1*f1+f2^2);f1-f2;f1+f2\" --input=\"{multiDimensional: 2}\" "
-                       "--metadata=paramId=1;paramId=2;paramId=3 input.grib output.grib"
+                       "--param-id=1;2;3 input.grib output.grib"
                     << std::endl;
     }
 
@@ -70,18 +71,14 @@ void MIRCompute::execute(const eckit::option::CmdArgs& args) {
     const param::ConfigurationWrapper param(args);
 
 
-    // create formula(s)/metadata(s)
-    auto args_split = [&args](const std::string& arg) {
-        std::string val;
-        args.get(arg, val);
-        return eckit::StringTools::split(";", val);
-    };
-
-    auto formulas = args_split("formula");
+    // arguments
+    std::vector<std::string> formulas;
+    args.get("formula", formulas);
     ASSERT(!formulas.empty());
 
-    auto metadatas = args_split("metadata");
-    ASSERT(metadatas.empty() || metadatas.size() == formulas.size());
+    std::vector<long> paramids;
+    args.get("param-id", paramids);
+    ASSERT(paramids.empty() || paramids.size() == formulas.size());
 
 
     // create input/output/save
@@ -98,7 +95,7 @@ void MIRCompute::execute(const eckit::option::CmdArgs& args) {
 
             param::RuntimeParametrisation run(param);
             run.set("formula", formulas[i]);
-            run.set("formula.metadata", metadatas.empty() ? "" : metadatas[i]);
+            run.set("formula.metadata", paramids.empty() ? "" : "paramId=" + std::to_string(paramids[i]));
 
             std::unique_ptr<action::FormulaAction> formula(new action::FormulaAction(run));
             formula->perform(ctx);
