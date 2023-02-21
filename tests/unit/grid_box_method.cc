85b40609a1ab0853e79c52e4633a85a9e3ecd936
Pedro Maciel <Pedro.Maciel@ecmwf.int>
2023-02-21 16:05:41 +0000

    MIR-601 Grid-box average periodicity handling, unit test

---
+++ tests/unit/grid_box_method.cc
@@ -0,0 +1,66 @@
+/*
+ * (C) Copyright 1996- ECMWF.
+ *
+ * This software is licensed under the terms of the Apache Licence Version 2.0
+ * which can be obtained at http://www.apache.org/licenses/LICENSE-2.0.
+ *
+ * In applying this licence, ECMWF does not waive the privileges and immunities
+ * granted to it by virtue of its status as an intergovernmental organisation nor
+ * does it submit to any jurisdiction.
+ */
+
+
+#include "eckit/testing/Test.h"
+
+#include "mir/repres/gauss/reduced/ReducedFromPL.h"
+#include "mir/util/BoundingBox.h"
+#include "mir/util/Domain.h"
+#include "mir/util/GridBox.h"
+#include "mir/util/Types.h"
+
+// define EXPECTV(a) log << "\tEXPECT(" << #a <<")" << std::endl; EXPECT(a)
+
+
+namespace mir::tests::unit {
+
+
+CASE("grid boxes: West-East periodicity") {
+    using Longitude = LongitudeDouble;
+
+
+    SECTION("reduced Gaussian grid (periodic)") {
+        const std::vector<long> pl{20, 20};
+
+        repres::RepresentationHandle r(new repres::gauss::reduced::ReducedFromPL(pl.size() / 2, pl));
+        ASSERT(r->domain().isPeriodicWestEast());
+
+        auto boxes = r->gridBoxes();
+        auto a     = boxes.front();
+        auto b     = boxes.back();
+        auto inc   = 360. / static_cast<double>(pl[0]);
+
+        EXPECT_EQUAL(Longitude(a.west()), Longitude(-inc / 2.));
+        EXPECT_EQUAL(Longitude(b.east()), Longitude(-inc / 2. + 360.));
+    }
+
+
+    SECTION("reduced Gaussian grid (non-periodic)") {
+        const std::vector<long> pl{20, 20};
+        util::BoundingBox bbox{90., 0., 0., 180.};
+
+        repres::RepresentationHandle r(new repres::gauss::reduced::ReducedFromPL(pl.size() / 2, pl, bbox));
+        ASSERT(!r->domain().isPeriodicWestEast());
+
+        for (const auto& box : r->gridBoxes()) {
+            EXPECT(bbox.contains({box.north(), box.west(), box.south(), box.east()}));
+        }
+    }
+}
+
+
+}  // namespace mir::tests::unit
+
+
+int main(int argc, char** argv) {
+    return eckit::testing::run_tests(argc, argv);
+}
