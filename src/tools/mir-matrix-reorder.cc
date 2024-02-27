f88d8347f3145309d3f9c2c8f457edb77c8aacbe
Pedro Maciel <Pedro.Maciel@ecmwf.int>
2024-02-27 12:53:12 +0000

    MIR-644 HEALPix::reorder

--- src/tools/mir-matrix-reorder.cc
+++ src/tools/mir-matrix-reorder.cc
@@ -14,13 +14,10 @@
 #include <numeric>
 #include <vector>
 
+#include "eckit/linalg/SparseMatrix.h"
 #include "eckit/option/CmdArgs.h"
 #include "eckit/option/FactoryOption.h"
-#include "eckit/option/SimpleOption.h"
 
-#include "mir/method/Method.h"
-#include "mir/param/ConfigurationWrapper.h"
-#include "mir/param/SimpleParametrisation.h"
 #include "mir/repres/Representation.h"
 #include "mir/repres/proxy/HEALPix.h"
 #include "mir/tools/MIRTool.h"
@@ -32,7 +29,7 @@
 namespace mir::tools {
 
 
-using Renumber = repres::proxy::HEALPix::Renumber;
+using Renumber = std::vector<size_t>;
 using Ordering = repres::proxy::HEALPix::Ordering;
 
 static util::once_flag ONCE;
@@ -141,21 +138,38 @@ private:
 };
 
 
-struct HEALPixRingToNested final : Reorder {
-    explicit HEALPixRingToNested(size_t N) {}
+struct HEALPix : Reorder {
+    explicit HEALPix(size_t N) :
+        N_(N), Nside_([N] {
+            auto Nside = static_cast<size_t>(std::sqrt(static_cast<double>(N) / 12.));
+            ASSERT_MSG(12 * Nside * Nside == N, "Expected N = 12 * Nside ** 2, got N=" + std::to_string(N));
+            return Nside;
+        }()) {}
+
+    size_t N() const { return N_; }
+    size_t Nside() const { return Nside_; }
+
+private:
+    const size_t N_;
+    const size_t Nside_;
+};
+
+
+struct HEALPixRingToNested final : HEALPix {
+    explicit HEALPixRingToNested(size_t N) : HEALPix(N) {}
     Renumber reorder(Ordering) override { NOTIMP; }
 };
 
 
-struct HEALPixNestedToRing final : Reorder {
-    explicit HEALPixNestedToRing(size_t N) {}
+struct HEALPixNestedToRing final : HEALPix {
+    explicit HEALPixNestedToRing(size_t N) : HEALPix(N) {}
     Renumber reorder(Ordering) override { NOTIMP; }
 };
 
 
 static const std::string IDENTITY = "identity";
 
-static const ReorderBuilder<Identity> __reorder1("identity");
+static const ReorderBuilder<Identity> __reorder1(IDENTITY);
 static const ReorderBuilder<HEALPixRingToNested> __reorder2("healpix-ring-to-nested");
 static const ReorderBuilder<HEALPixNestedToRing> __reorder3("healpix-nested-to-ring");
 
@@ -171,7 +185,8 @@ struct MIRMatrixReorder : MIRTool {
 
     void usage(const std::string& tool) const override {
         Log::info() << "\n"
-                    << "Usage: " << tool
+                       "Usage: "
+                    << tool
                     << " [--reorder-rows=...]"
                        " [--reorder-cols=...]"
                        " input-matrix output-matrix"
@@ -183,13 +198,22 @@ struct MIRMatrixReorder : MIRTool {
 
 
 void MIRMatrixReorder::execute(const eckit::option::CmdArgs& args) {
-    auto matin  = args(0);
-    auto matout = args(1);
+    // access input matrix
+    eckit::linalg::SparseMatrix in;
+    in.load(args(0));
 
-    std::unique_ptr<Reorder> rows(ReorderFactory::build(args.getString("reorder-rows"), 42));
-    std::unique_ptr<Reorder> cols(ReorderFactory::build(args.getString("reorder-cols"), 42));
 
-    NOTIMP;  // TODO
+    // renumbering methods
+    std::unique_ptr<Reorder> rows(ReorderFactory::build(args.getString("reorder-rows"), in.rows()));
+    std::unique_ptr<Reorder> cols(ReorderFactory::build(args.getString("reorder-cols"), in.cols()));
+
+
+    // expand triplets, renumbering directly
+    // SparseMatrix(Size rows, Size cols, const std::vector<Triplet>& triplets);
+
+
+    // compress triplets
+    // NOTIMP;  // TODO
 }
 
 
