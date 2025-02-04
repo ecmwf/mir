0a6c6847624e95198283ff77d977f435c3e13f3e
Vojta Tuma <vojta.tuma@gmail.com>
2025-02-04 22:41:31 +0100

    Dependency typo

--- build_chain.sh
+++ build_chain.sh
@@ -31,7 +31,7 @@ else
 fi
 
 # mir-python prereqs
-uv pip install $EXTRA_PIP eccodeslib eckit mirlib
+uv pip install $EXTRA_PIP eccodeslib eckitlib mirlib
 PRF=".venv/lib/python3.11/site-packages"
 if [ "$(uname)" == "Darwin" ] ; then L="lib" ; else L="lib64" ; fi
 export MIR_LIB_DIR="$PRF/eckitlib/$L:$PRF/eccodeslib/$L:$PRF/mirlib/$L"
