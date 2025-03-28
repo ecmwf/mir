bb10477e76448524b0b2825b56acfd97b9feb22a
Pedro Maciel <Pedro.Maciel@ecmwf.int>
2025-03-28 10:46:43 +0000

    MIR-687 mir-version

--- src/tools/mir-version.cc
+++ src/tools/mir-version.cc
@@ -10,18 +10,42 @@
  */
 
 
+#include <map>
+#include <numeric>
 #include <string>
 
 #include "eckit/log/JSON.h"
 #include "eckit/option/CmdArgs.h"
 #include "eckit/option/SimpleOption.h"
+#include "eckit/system/Library.h"
+#include "eckit/system/LibraryManager.h"
 
 #include "mir/api/mir_config.h"
 #include "mir/tools/MIRTool.h"
+#include "mir/util/Grib.h"
 #include "mir/util/Log.h"
 
 
 namespace mir::tools {
+
+
+struct Libraries : std::map<std::string, std::string> {
+    Libraries() {
+        constexpr size_t sha1len = 8;
+
+        using eckit::system::LibraryManager;
+        for (const auto& lib_name : LibraryManager::list()) {
+            const auto& lib = LibraryManager::lookup(lib_name);
+
+            emplace(lib_name + " version", lib.version());
+            emplace(lib_name + " git-sha1", lib.gitsha1(sha1len));
+            emplace(lib_name + " home", lib.libraryHome());
+        }
+
+        emplace("eccodes version", ECCODES_VERSION_STR);
+        emplace("eccodes git-sha1", std::string(codes_get_git_sha1()).substr(0, sha1len));
+    }
+};
 
 
 struct MIRVersion : MIRTool {
@@ -80,6 +104,14 @@ struct MIRVersion : MIRTool {
             out << "HAVE_TESSELATION" << HAVE_TESSELATION;
             out.endObject();
 
+            out << "features";
+            out.startObject();
+            static const Libraries libs;
+            for (const auto& [key, val] : libs) {
+                out << key << val;
+            }
+            out.endObject();
+
             out.endObject();
             return;
         }
@@ -111,6 +143,19 @@ struct MIRVersion : MIRTool {
         out << "\n  HAVE_PROJ        : " << HAVE_PROJ;
         out << "\n  HAVE_OMP         : " << HAVE_OMP;
         out << "\n  HAVE_TESSELATION : " << HAVE_TESSELATION;
+        out << '\n';
+
+        out << "\nLibraries:";
+        static const Libraries libs;
+        auto key_max = std::accumulate(libs.begin(), libs.end(), static_cast<size_t>(0),
+                                       [](size_t max, const auto& kv) { return std::max(max, kv.first.size()); });
+
+        for (const auto& [key, val] : libs) {
+            auto key_str = key;
+            key_str.resize(key_max);
+
+            out << "\n  " << key_str << " : " << val;
+        }
         out << std::endl;
     };
 };
