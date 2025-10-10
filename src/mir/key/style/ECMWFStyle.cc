bc786599c2dc3f67e002f7d25f651b617db34087
Pedro Maciel <Pedro.Maciel@ecmwf.int>
2025-10-10 09:04:23 +0100

    MIR-592 MIR-681 MIR-690 MIR-695 Improved wind handling at the poles on inverse spectral transforms from U/V

--- src/mir/key/style/ECMWFStyle.cc
+++ src/mir/key/style/ECMWFStyle.cc
@@ -331,10 +331,6 @@ void ECMWFStyle::sh2grid(action::ActionPlan& plan) const {
             }
         }
 
-        if (uv2uv) {
-            plan.add("filter.adjust-winds-at-poles");
-        }
-
         if (vod2uv || uv2uv) {
             if (rotation) {
                 plan.add("filter.adjust-winds-directions");
