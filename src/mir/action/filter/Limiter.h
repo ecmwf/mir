cd92fba345424060f1417b09dda883670f40a8c9
Pedro Maciel <pmaciel@ecmwf.int>
2026-02-16 17:23:55 +0000

    MIR-704 Field values limiter

---
+++ src/mir/action/filter/Limiter.h
@@ -0,0 +1,36 @@
+/*
+ * (C) Copyright 1996- ECMWF.
+ *
+ * This software is licensed under the terms of the Apache Licence Version 2.0
+ * which can be obtained at http://www.apache.org/licenses/LICENSE-2.0.
+ *
+ * In applying this licence, ECMWF does not waive the privileges and immunities
+ * granted to it by virtue of its status as an intergovernmental organisation nor
+ * does it submit to any jurisdiction.
+ */
+
+
+#pragma once
+
+#include "mir/action/plan/Action.h"
+
+
+namespace mir::action::filter {
+
+
+class Limiter final : public Action {
+public:
+    explicit Limiter(const param::MIRParametrisation&);
+
+private:
+    double minimum_;
+    double maximum_;
+
+    bool sameAs(const Action&) const override;
+    const char* name() const override;
+    void print(std::ostream&) const override;
+    void execute(context::Context&) const override;
+};
+
+
+}  // namespace mir::action::filter
