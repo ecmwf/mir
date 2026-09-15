243654c61b401085fb3bc5d8c8af5e28463c4836
Ioan Hadade <ioan.hadade@ecmwf.int>
2026-09-15 13:06:52 +0000

    Apply previous fix with local timer everywhere

--- src/mir/output/GribOutput.cc
+++ src/mir/output/GribOutput.cc
@@ -594,7 +594,7 @@ size_t GribOutput::save_with_metkit(const param::MIRParametrisation& param, cont
         ASSERT(h);
 
         {
-            auto timing(ctx.statistics().saveTimer());
+            util::MIRStatistics::AutoTiming timing(saveTimer);
             out(h->messageData().data(), h->messageSize(), true);
         }
 
